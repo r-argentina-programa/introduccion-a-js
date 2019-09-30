6b1da2249cfbef46ee401f0e38debb1a5fdfcf7e
tomas95go <tomas95go@gmail.com>
2019-09-30 20:03:39 -0300

    Actualizar tarea-clase-4.js
    He traducido desde la consiga 13 a la consigna 44, con excepción de la consigna 43. Algunas consignas tienen una nota puesto a que no estoy seguro de si la traducción por la que opte es correcta.

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -17,43 +17,53 @@ usando  elementos de programación simples y clásicos.
  Desafío de programación #9: Crea una función que convierta de Fahrenheit a Celsius
  Desafío de programación #10: Calcula la suma de todos los números en un array de números
  Desafío de programación #11: Calcula el promedio de todos los números en un array de números.
- Desafío de programación #12: Crea una función que reciba un array de números y devuelva un array conteniendo solo los números positivos
- Coding challenge #12: Create a function that receives an array of numbers and returns an array containing only the positive numbers
- Coding challenge #13: Find the maximum number in an array of numbers
- Coding challenge #14: Print the first 10 Fibonacci numbers without recursion
+ Desafío de programación #12: Crea una función que reciba un array de números y devuelva un array conteniendo solo los números positivos.
+ Desafío de programación #13: Encontrar el número de mayor valor en un arreglo(array) de números.
+ Desafío de programación #14: Mostrar en pantalla(imprimir) los 10 primeros numeros de la sucesión de Fibonacci sin utilizar recursividad.
+ Desafío de programación #15: Crear una función que encuentre el elemento n en la sucesión de Fibonacci utilizando recursividad. 
+ (Nota #15, no estoy seguro de la traducción, favor de revisar.)
  Coding challenge #15: Create a function that will find the nth Fibonacci number using recursion
- Coding challenge #16: Create a function that will return a Boolean specifying if a number is prime
- Coding challenge #17: Calculate the sum of digits of a positive integer number
- Coding challenge #18: Print the first 100 prime numbers
+ Desafío de programación #16: Crear una función que retorne un valor booleano si un número es primo.
+ Desafío de programación #17: Calcular la suma de los dígitos de un número entero positivo.
+ Desafío de programación #18: Mostrar en pantalla (imprimir) los primeros 100 números primos.
  Coding challenge #19: Create a function that will return in an array the first "nPrimes" prime numbers greater than a particular number "startAt"
+ Desafío de programación #19: Crear una función que retorne un arreglo(array) con los primeros números primos que son mayores a un número en particular.
+ (Nota #19, no estoy seguro de la traducción, favor de revisar).
+ Desafío de programación #20: Rotar un arreglo(array) una posición, hacia la izquierda.
  Coding challenge #20: Rotate an array to the left 1 position
+ Desafío de programación #21: Rotar un arreglo(array) una posición, hacia la derecha.
  Coding challenge #21: Rotate an array to the right 1 position
- Coding challenge #22: Reverse an array
- Coding challenge #23: Reverse a string
- Coding challenge #24: Create a function that will merge two arrays and return the result as a new array
+ (Nota #20 y Nota #21, no estoy seguro de la traducción, favor de revisar).
+ Desafío de programación #22: Revertir un arreglo(array).
+ Desafío de programación #23: Revertir una cadena de cáracteres (string).
+ Desafío de programación #24: Crear una función que combine dos arrays (arreglos) y retorne el resultado en un nuevo array.
  Coding challenge #25: Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
- Coding challenge #26: Create a function that will receive two arrays and will return an array with elements that are in the first array but not in the second
+ Desafío de programación #25: Crear una función que reciba dos arreglos(arrays) como argumento y retorne un arreglo(array) compuesto por todos los números que se encuentren:
+ en el primer arreglo(array) o en el segundo arreglo(array) pero no en ambos.
+ Desafío de programación #26: Crear una función que reciba dos arreglos(arrays) y que retorne un arreglo que contenga los elementos del primer arreglo(array) pero no los del segundo arreglo(array).
  Coding challenge #27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
- Coding challenge #27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
-Coding challenge #28: Calculate the sum of first 100 prime numbers
-Coding challenge #29: Print the distance between the first 100 prime numbers
-Coding challenge #30-a: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
-Coding challenge #30-b: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
-Coding challenge #31-a. Create a function that will return the number of words in a text
-Coding challenge #31-b. Create a function that will return the number of words in a text
-Coding challenge #32. Create a function that will capitalize the first letter of each word in a text
-Coding challenge #33. Calculate the sum of numbers received in a comma delimited string
-Coding challenge #34. Create a function that will return an array with words inside a text
-Coding challenge #35. Create a function to convert a CSV text to a “bi-dimensional” array
-Coding challenge #36. Create a function that converts a string to an array of characters
-Coding challenge #37. Create a function that will convert a string in an array containing the ASCII codes of each character
-Coding challenge #38. Create a function that will convert an array containing ASCII codes in a string
-Coding challenge #39. Implement the Caesar cypher
-Coding challenge #40. Implement the bubble sort algorithm for an array of numbers
+ Desafío de programación #27: Crear una función que reciba un arreglo(array) de números como argumento y retorne un nuevo array(arreglo) con los elementos distintos.
+ (Nota #27, no estoy seguro de la traducción, favor de revisar).
+Coding challenge #27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+Desafío de programación #28: Calcular la suma de los 100 primeros números primos.
+Desafío de programación #29: Mostrar en pantalla (print/imprimir) la distancia que existe entre los 100 primeros números primos.
+Desafío de programación #30: Crear una función que sume dos números positivos de tamaño indefinido.
+Los números son recibidos como una cadena de caracteres(string) y el resultado deberia ser también una cadena de caracteres(string).
+Desafío de programación #31: Crear una función que retorne el número de palabras en un texto.
+Desafío de programación #32: Crear una función que convertirá en mayúscula la primera letra de cada palabra en un texto.
+Desafío de programación #33: Calcular la suma de los números recibidos de una cadena de cáracteres(string) separados por comas.
+Desafío de programación #34: Crear una función que retorne un arreglo(array) con palabras dentro de un texto.
+Desafío de programación #35: Crear una función que convierta un texto CSV a un array(arreglo) bi-dimensional.
+Desafío de programación #36: Crear una función que convierta una cadena de caracteres(string) a un array(arreglo) de caracteres.
+Desafío de programación #37: Crear una función que convierta una cadena de caracteres(string) a un array que contenga el código ASCII de cada caracter.
+Desafío de programación #38: Crear una función que convierta un arreglo que contiene códigos ASCII a una cadena de caracteres(string).
+Desafío de programación #30: Implementar el cifrado César.
+Desafío de programación #40: Implementar el algoritmo: ordenamiento de burbuja(bubble sort), en un arreglo(array) de números.
 Coding challenge #41. Create a function to calculate the distance between two points defined by their x, y coordinates
-Coding challenge #42. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting
+Desafío de programación #41: Crear una función para calcular la distancia entre dos puntos definidos por sus coordenadas x,y.
+Desafío de programación #42: Crear una función que retorne un valor booleano el cual indica si 2 circulos definidos por un centro y un radio se intersectan.
 Coding challenge 43. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
-Coding challenge #44. Create a function that will convert a string containing a binary number into a number
+Desafío de programación #44: Crear una función que convierta una cadena de caracteres(string) de números binarios a un número.
 Coding challenge #45. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
 Coding challenge #46-a. Find the maximum number in a jagged array of numbers or array of numbers
 Coding challenge #46-b. Find the maximum number in a jagged array of numbers or array of numbers
