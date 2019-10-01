f33f7369a0db65e360b89ea2bb9584da9b04e373
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2019-10-01 13:56:08 +1000

    Mueve las tareas de algoritmos a un archivo aparte

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -1,67 +1,18 @@
-//Link original y créditos a: https://github.com/CodeGuppyPrograms/CodingChallenges
+// TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
 
-/*
-Estos desafíos de coding están enfocados para los principiantes, por lo tanto las soluciones fueron implementadas
-usando  elementos de programación simples y clásicos.
-*/
 
-/*
- Desafío de programación #1: Imprimí los números del 1 al 10
- Desafìo de programación  #2: Imprimí los números impares del 1 al 100
- Desafío de programación #3: Imprimí la tabla de multiplicación del 7
- Desafío de programación #4: Imprimí todas las tablas de multiplicación del 1 al 10
- Desafío de programación #5: Calcula el resultado de la suma de los numeros del 1 al 10.
- Desafío de programación #6: Calcula 10!
- Desafío de programación #7: Calcula la suma de todos los números impares mayores que 10 y menores que 30
- Desafío de programación #8: Crea una función que convierta de Celsius a Fahrenheit
- Desafío de programación #9: Crea una función que convierta de Fahrenheit a Celsius
- Desafío de programación #10: Calcula la suma de todos los números en un array de números
- Desafío de programación #11: Calcula el promedio de todos los números en un array de números.
- Desafío de programación #12: Crea una función que reciba un array de números y devuelva un array conteniendo solo los números positivos
- Coding challenge #12: Create a function that receives an array of numbers and returns an array containing only the positive numbers
- Coding challenge #13: Find the maximum number in an array of numbers
- Coding challenge #14: Print the first 10 Fibonacci numbers without recursion
- Coding challenge #15: Create a function that will find the nth Fibonacci number using recursion
- Coding challenge #16: Create a function that will return a Boolean specifying if a number is prime
- Coding challenge #17: Calculate the sum of digits of a positive integer number
- Coding challenge #18: Print the first 100 prime numbers
- Coding challenge #19: Create a function that will return in an array the first "nPrimes" prime numbers greater than a particular number "startAt"
- Coding challenge #20: Rotate an array to the left 1 position
- Coding challenge #21: Rotate an array to the right 1 position
- Coding challenge #22: Reverse an array
- Coding challenge #23: Reverse a string
- Coding challenge #24: Create a function that will merge two arrays and return the result as a new array
- Coding challenge #25: Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
- Coding challenge #26: Create a function that will receive two arrays and will return an array with elements that are in the first array but not in the second
- Coding challenge #27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
- Coding challenge #27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
-Coding challenge #28: Calculate the sum of first 100 prime numbers
-Coding challenge #29: Print the distance between the first 100 prime numbers
-Coding challenge #30-a: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
-Coding challenge #30-b: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
-Coding challenge #31-a. Create a function that will return the number of words in a text
-Coding challenge #31-b. Create a function that will return the number of words in a text
-Coding challenge #32. Create a function that will capitalize the first letter of each word in a text
-Coding challenge #33. Calculate the sum of numbers received in a comma delimited string
-Coding challenge #34. Create a function that will return an array with words inside a text
-Coding challenge #35. Create a function to convert a CSV text to a “bi-dimensional” array
-Coding challenge #36. Create a function that converts a string to an array of characters
-Coding challenge #37. Create a function that will convert a string in an array containing the ASCII codes of each character
-Coding challenge #38. Create a function that will convert an array containing ASCII codes in a string
-Coding challenge #39. Implement the Caesar cypher
-Coding challenge #40. Implement the bubble sort algorithm for an array of numbers
-Coding challenge #41. Create a function to calculate the distance between two points defined by their x, y coordinates
-Coding challenge #42. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting
-Coding challenge 43. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
-Coding challenge #44. Create a function that will convert a string containing a binary number into a number
-Coding challenge #45. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
-Coding challenge #46-a. Find the maximum number in a jagged array of numbers or array of numbers
-Coding challenge #46-b. Find the maximum number in a jagged array of numbers or array of numbers
-Coding challenge #47. Deep copy a jagged array with numbers or other arrays in a new array
-Coding challenge #48. Create a function to return the longest word(s) in a string
-Coding challenge #49. Shuffle an array of strings
-Coding challenge #50. Create a function that will receive n as argument and return an array of n unique random numbers from 1 to n.
-Coding challenge #51. Find the frequency of characters inside a string. Return the result as an array of objects. Each object has 2 fields: character and number of occurrences.
-Coding challenge #52. Calculate Fibonacci(500) with high precision (all decimals)
-Coding challenge #53. Calculate 70! with high precision (all decimals)
- */
+// TAREA: Usando un bucle 'while', decile a tu computadora que registre los números de
+//       diez a uno.
+
+
+// TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
+// Cuenta del 1 al 50 e imprime los números:
+// * Si un número es múltiplo de tres, imprime 'Fizz'.
+// * Si es un múltiplo de 5, imprime 'Buzz'.
+// * Si es un múltiplo de 3 y 5, imprime 'FizzBuzz'.
+// * Para todo lo demás, imprime el número mismo.
+// NOTA: Es posible que desees utilizar el operador aritmético modulo (%):
+// Calcula el resto al dividir.
+// 10% 3 = 1 - en 10 tenemos 3 * 3 + 1
+// 16% 4 = 0 - en 16 tenemos 4 * 4
+// 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
