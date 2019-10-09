637939270b6e91a5182cd6ee3f99bdc0c9c6392a
vitipe <victorcasbas@gmail.com>
2019-10-09 15:55:05 -0300

    Cambios varios para clase 5

--- js/nivel3-mio.js
+++ js/nivel3-mio.js
@@ -3,9 +3,12 @@
 /*
     Introducción
     ============
+
     Bieeeeen, has llegado lejos en tan poco tiempo!
+
     Pero todavía tenemos más cosas para vos. Antes de comenzar, abrí index.html, y
     reemplazá nuestro script de nivel2.js al que vamos a utilizar ahora, — nivel3.js.
+
     TIP: Si querés utilizar multiples archivos .js, simplemente
     agregá más etiquetas <script>.
 */
@@ -13,27 +16,40 @@
 /*
     Hablemos un poco más de HTML, CSS y de como podemos interactuar con ellos
     en JavaScript.
+
+
     Hypertext Markup Language (HTML)
     --------------------------------
     Como te habrás dado cuenta, HTML está dividido en elementos que se ven algo así:
+
     <header></header>
     <p></p>
     <img />
+
     A estos elementos los llamamos "tags". Cada elemento de la página tiene un tag que abre y otro que
     cierra. (NOTA: Algunos tags, como <img />, no necesitan ser
     cerrados con otro tag.)
+
     La etiqueta que engloba a todas las demás en un archivo HTML es <html>.
+
     Dentro de la etiqueta <html> encontrarás un tag <head> y un tag <body>.
+
     En <head> metemos los metadatos, el título de la página y los links a los
     archivos CSS. La etiqueta <body> contiene nuestro contenido.
+
     Para una lista completa de las etiquetas HTML, podés mirar esta lista:
     http://htmldog.com/references/html/tags/
+
     En HTML, los tags pueden contener atributos:
     <div class="opciones"></div>
+
     Esta etiqueta div contiene un atributo llamado "class", el cual tiene como valor: "opciones".
+
+
     Cascading Style Sheets (CSS)
     ----------------------------
     CSS describe como se ven los elementos HTML.
+
     Los archivos CSS consisten en "bloques de declaración". Cada bloque de
     declaración está compuesto de un "selector" y de un conjunto de reglas de estilo
     visuales. Una declaración se ve así:
@@ -43,24 +59,33 @@
       nombre-de-estilo: valor;
       nombre-de-estilo: valor;
     }
+
     Los selectores especifican sobre que elementos son aplicados los
     estilos visuales.
+
     Los selectores más básicos se refieren a los elementos mediante su nombre de etiqueta,
     se ven algo así:
+
     body {
       background-color: white;
     }
+
     Los selectores pueden también referirse a los elementos mediante su atributo
     de "clase", como esto:
+
     .settings {
       margin: 0;
     }
+
     o por su ID, así:
+
     #logo {
       text-align: center;
     }
+
     La lista de las propiedades de CSS es muy grande, podés leer más acá:
     https://www.w3.org/TR/CSS21/propidx.html
+
     No te preocupés, no hace falta recordar todo esto ahora mismo!
 */
 
@@ -74,15 +99,20 @@
 /*
     Elementos de Acceso
     ==================
+
     Document Object Model (DOM)
     ---------------------------
     El DOM es la representación en JavaScript del elemento HTML que está siendo
     utilizado. EL DOM está disponible bajo una variable global especial llamada
     "document". Podemos obtener nodos específicos (elementos de página) con el
     metodo DOM: '.querySelector'.
+
     Obtengamos nuestro link de Twitter desde la página.
+
     Ejemplo:
+
     const nuestroTwitter = document.querySelector('.twitter');
+
     // Podemos guardar elementos de la página en variables, al igual que como cualquier otro valor!
     // De todas formas, fijate que un elemento de página es un objeto,
     // el cual es un "tipo de referencia", así como los arrays (mirá el nivel 2).
@@ -94,26 +124,32 @@
 //       variable llamada nuestroTitulo.
 //       Utilizá console.log para ver lo que obtuviste!
 
+const nuestroTwitter = document.querySelector('.twitter');
+console.log(nuestroTwitter);
 
-
-
+const nuestroTitulo = document.querySelector('h1');
+console.log(nuestroTitulo);
 
 
 /*
     Obteniendo elementos similares.
     ========================
+
     También podemos obtener todos los elementos con la misma etiqueta. En nuestro
     pie de página tenemos una lista desordenada (<ul>), con tres elementos de lista (<li>) adentro.
     Obtengamoslos todos con un array de los elementos de la página. Cuando querramos obtener multiples elementos,
     podemos utilizar el método '.querySelectorAll', el cual nos dará una lista con las coincidencias.
+
     Ejemplo:
+
     // Esto obtendrá todos los <li> de la página.
     const mediaLinks = document.querySelectorAll('li');
 */
 
 // TAREA: Obtené todos los elementos <li> de la página en una variable llamada mediaLinks.
 
-
+const mediaLinks = document.querySelectorAll('li');
+console.log(mediaLinks);
 
 
 
@@ -121,7 +157,7 @@
 // TAREA: Ahora utilizá console.log para ver la cantidad de 
 // elementos li que hay con mediaLinks.length
 
-
+console.log(mediaLinks.length);
 
 
 
@@ -130,7 +166,9 @@
 //      sobre cada item de mediaLinks y mostralos en pantalla con console.log
 
 
-
+for (let i = 0; i < mediaLinks.length; i++) {
+    console.log(mediaLinks[i]);
+}
 
 
 
@@ -141,7 +179,9 @@
     Ok, hasta acá todo bien. Peeeroo, ¿que pasa si queremos obtener SOLO el texto
     de nuestra etiqueta 'h1'?
     Los elementos de página tienen una propiedad para esto: '.textContent'.
+
     Ejemplo:
+
     nuestroTwitter.textContent;
     // Obtendremos el texto: 'Twitter: @MusesCodeJSSyd @MusesCodeJSMelb @MCJSHobart @MCJSPerth @BrisMuses'.
 */
@@ -150,16 +190,19 @@
 // y utilizá console.log para mostrarlo.
 
 
-
+console.log(nuestroTwitter.textContent);
 
 
 
 /*
     Editar el contenido de la página
     ====================
+
     Podemos simplemente cambiar el contenido de las étiquetas utilizando la propiedad que 
     vimos recién, '.textContent'.
+
     Ejemplo:
+
     nuestroTwitter.textContent = '@ButenkoMe';
     console.log(nuestroTwitter.textContent);
     // Adiviná que es lo que vamos a ver en la página y en la consola!
@@ -168,7 +211,8 @@
 // TAREA: Hagamos un nuevo título! Cambiá el contenido de nuestro 'h1' y ponele lo que quieras.
 
 
-
+nuestroTitulo.textContent = "Hooooola vieja";
+console.log(nuestroTitulo.textContent);
 
 
 /*
@@ -176,28 +220,37 @@
     ==================
     
     También podemos cambiar y establecer atributos en nuestros elementos.
+
     Ejemplo:
+
     const nuestroTwitter = document.querySelector('.twitter');
     ourTwitter.id = "sorpresa";
 */
 
 // TAREA: Actualizá el valor del atributo 'src' de nuestra etiqueta 'img' a "img/kittens.jpeg".
 
+// const nuestraImagen = document.querySelector('.img');
+// nuestraImagen.src = "img/kittens.jpeg";
 
-
+// QUe onda aca? me parece que colgaron en un poner una imagen para que ande esto
+//Sí, colgaron.
 
 
 
 /*
     Editando estilos
     ==============
+
     Cambiemos algunos estilos. Los elementos de página tienen una propiedad '.style'.
     Podemos asignarle estilos a esta propiedad utilizando los mismos nombres que en
     los archivos CSS.
+
     (Si una propiedad tiene '-' en su nombre, como — font-size —, entonces
     el guión será eliminado y las siguiente palabra comenzara con una letra mayúscula, y pasará
     a ser fontSize).
+
     Ejemplo:
+
     const nuestroTwitter = document.querySelector('.twitter');
     ourTwitter.style.backgroundColor = 'white';
 */
@@ -205,6 +258,7 @@
 // Tarea: Obtené cualquier elemento de la página y cambiale algunos estilos.
 
 
+nuestroTwitter.style.backgroundColor = 'red';
 
 
 
@@ -212,11 +266,15 @@
 /*
    Creando nuevos nodos (Elementos)
     =============================
+
     El objeto "document" también nos da maneras de crear nodos desde cero.
+
     document.createElement('div'); // crea un nuevo elemento llamado 'div'.
     document.createTextNode('foobar'); // crea un nuevo texto que contiene 'foobar'
     existingNode.appendChild(newNode); // agrega un newNode al final de existingNode.
+
     Ejemplo:
+
     const nodoPagina = document.querySelector('body');
     const nuevoParrafo = document.createElement('p');
     const textoParrafo = document.createTextNode('Yeeee!');
