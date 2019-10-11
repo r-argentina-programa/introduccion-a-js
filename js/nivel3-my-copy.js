85da948bddbd31dce2d21d5fe6b153789910c126
jmoralesg <johanamgff@gmail.com>
2019-10-11 18:51:50 +1100

    tarea clase 5

--- js/nivel3-my-copy.js
+++ js/nivel3-my-copy.js
@@ -89,12 +89,10 @@
     No te preocupés, no hace falta recordar todo esto ahora mismo!
 */
 
-
 /*
     Uff, muchas cosas nuevas! Volvamos a JavaScript y veamos como podemos
     interactuar con HTML.
 */
-
 
 /*
     Elementos de Acceso
@@ -124,11 +122,6 @@
 //       variable llamada nuestroTitulo.
 //       Utilizá console.log para ver lo que obtuviste!
 
-
-
-
-
-
 /*
     Obteniendo elementos similares.
     ========================
@@ -146,27 +139,17 @@
 
 // TAREA: Obtené todos los elementos <li> de la página en una variable llamada mediaLinks.
 
-
-
-
-
-
-// TAREA: Ahora utilizá console.log para ver la cantidad de 
+// TAREA: Ahora utilizá console.log para ver la cantidad de
 // elementos li que hay con mediaLinks.length
 
-
-
-
-
+const mediaLinks = document.querySelectorAll('li');
+console.log(mediaLinks.length);
 
 // TAREA: ¿Te acordás de los bucles del nivel 2? Usando lo que sabés de ellos, realizá iteraciones
 //      sobre cada item de mediaLinks y mostralos en pantalla con console.log
-
-
-
-
-
-
+for (let i = 0; i < mediaLinks.length; i++) {
+  console.log(mediaLinks[i]);
+}
 /*
     Propiedades de los elementos
     ==================
@@ -183,11 +166,10 @@
 
 // TAREA: Obtené el contenido de nuestro elemento 'h1'
 // y utilizá console.log para mostrarlo.
-
-
-
-
-
+const titulo = document.querySelector('h1');
+console.log(titulo.textContent);
+console.log(titulo.innerHTML);
+console.log(titulo.innerText);
 
 /*
     Editar el contenido de la página
@@ -204,11 +186,7 @@
 */
 
 // TAREA: Hagamos un nuevo título! Cambiá el contenido de nuestro 'h1' y ponele lo que quieras.
-
-
-
-
-
+titulo.textContent = 'chau';
 /*
     Editando atributos
     ==================
@@ -222,12 +200,7 @@
 */
 
 // TAREA: Actualizá el valor del atributo 'src' de nuestra etiqueta 'img' a "img/kittens.jpeg".
-
-
-
-
-
-
+document.querySelector('img').src = 'img/kittens.jpeg';
 /*
     Editando estilos
     ==============
@@ -247,12 +220,7 @@
 */
 
 // Tarea: Obtené cualquier elemento de la página y cambiale algunos estilos.
-
-
-
-
-
-
+titulo.style.border = '5px solid gray';
 /*
    Creando nuevos nodos (Elementos)
     =============================
@@ -277,12 +245,9 @@
 //       en nuestra cabecera.
 //
 // P.S. También les podés dar estilos al nuevo nodo que creaste.
-
-
-
-
-
-
+const nodoImagen = document.createElement('img');
+nodoImagen.src = 'img/woman_bw.jpg';
+document.querySelector('header').appendChild(nodoImagen);
 ////////////////////////////////////////////////////////////////////////////
 // ¡Felicidades! ¡Has terminado el Nivel 3 de JavaScript Básico!          //
 // Levántate, estira las piernas y celebra tu logro.                      //
