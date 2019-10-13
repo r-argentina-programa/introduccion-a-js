29386dfb8105316ca32a471c3b42a1d7ea24701e
vitipe <victorcasbas@gmail.com>
2019-10-13 17:48:39 -0300

    Completado tarea 3 clase 5 y plantilla tarea 4 de clase 5

--- tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
+++ tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
@@ -7,15 +7,47 @@
 // <strong> pre-creado el tiempo total de los videos.
 
 
+function calcularDuracion(horasArray, minutosArray, segundosArray) {
+    let totalHoras = 0;
+    let totalMinutos = 0;
+    let totalSegundos= 0;
+    for (let i = 0; i<horasArray.length; i++) {
+        if (parseInt(horasArray[i].value)) {
+            resultadoHoras = totalHoras += (parseInt(horasArray[i].value));
+        }
+    } 
+    for (let i = 0; i<minutosArray.length; i++) {
+        if (parseInt(minutosArray[i].value)) {
+            resultadoMinutos = totalMinutos += (parseInt(minutosArray[i].value));
+        }
+    } 
+        if (resultadoMinutos >= 60) {
+        resultadoHoras += Math.floor(resultadoMinutos/60);
+        resultadoMinutos = resultadoMinutos % 60;
+        } 
+    for (let i = 0; i<segundosArray.length; i++) {
+        if (parseInt(segundosArray[i].value)) {
+            resultadoSegundos = totalSegundos += (parseInt(segundosArray[i].value));
+        }
+    } 
+        if (resultadoSegundos >= 60) {
+            resultadoMinutos += Math.floor(resultadoSegundos/60);
+            resultadoSegundos = resultadoSegundos % 60;
+    } 
+    return "Tiempo total: " + resultadoHoras + " horas, " + resultadoMinutos + " minutos, " + resultadoSegundos + " segundos."
+}
+
 
 const $calculoHoras = document.querySelector('#calculo-tiempo-total');
 
 $calculoHoras.onclick = function(){
     let horasArray = document.getElementsByClassName('tiempo-horas');
-    let totalHoras = 0;
-    for (let i = 0; i<horasArray.length; i++) {
-        if (parseInt(horasArray[i].value))
-            totalHoras += (parseInt(horasArray[i].value));
-    }
-    document.querySelector('#resultado').innerText = totalHoras + " horas";
+    let minutosArray = document.getElementsByClassName('tiempo-minutos');
+    let segundosArray = document.getElementsByClassName('tiempo-segundos');
+    resultadoTotal = calcularDuracion(horasArray, minutosArray, segundosArray);
+
+    document.querySelector('#resultado').innerText = resultadoTotal;
+   
+    return false;
 }
+
---
+++ tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
@@ -0,0 +1,7 @@
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
