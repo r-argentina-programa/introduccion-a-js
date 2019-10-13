0fa9ed213e8e37b25409661e4a8291562903a186
vitipe <victorcasbas@gmail.com>
2019-10-13 18:05:02 -0300

    Acomodando código para que quede legible

--- tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
+++ tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
@@ -15,26 +15,21 @@ function calcularDuracion(horasArray, minutosArray, segundosArray) {
         if (parseInt(horasArray[i].value)) {
             resultadoHoras = totalHoras += (parseInt(horasArray[i].value));
         }
-    } 
-    for (let i = 0; i<minutosArray.length; i++) {
+    } for (let i = 0; i<minutosArray.length; i++) {
         if (parseInt(minutosArray[i].value)) {
             resultadoMinutos = totalMinutos += (parseInt(minutosArray[i].value));
         }
-    } 
-    if (resultadoMinutos >= 60) {
+    } if (resultadoMinutos >= 60) {
         resultadoHoras += Math.floor(resultadoMinutos/60);
         resultadoMinutos = resultadoMinutos % 60;
-    } 
-    for (let i = 0; i<segundosArray.length; i++) {
+    } for (let i = 0; i<segundosArray.length; i++) {
         if (parseInt(segundosArray[i].value)) {
             resultadoSegundos = totalSegundos += (parseInt(segundosArray[i].value));
         }
-    } 
-    if (resultadoSegundos >= 60) {
+    } if (resultadoSegundos >= 60) {
             resultadoMinutos += Math.floor(resultadoSegundos/60);
             resultadoSegundos = resultadoSegundos % 60;
-    } 
-    return "Tiempo total: " + resultadoHoras + " horas, " + resultadoMinutos + " minutos, " + resultadoSegundos + " segundos."
+    } return "Tiempo total: " + resultadoHoras + " horas, " + resultadoMinutos + " minutos, " + resultadoSegundos + " segundos."
 }
 
 
