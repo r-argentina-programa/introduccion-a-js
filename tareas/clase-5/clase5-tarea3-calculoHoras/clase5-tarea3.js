0fd8d7e03a23662c9528cb6f4da7ae986882d0c2
vitipe <victorcasbas@gmail.com>
2019-10-14 20:31:13 -0300

    Cambiando .innerText por .textContent

--- tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
+++ tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
@@ -46,7 +46,7 @@ $calculoHoras.onclick = function() {
     let segundosArray = document.getElementsByClassName('tiempo-segundos');
     resultadoTotal = calcularDuracion(horasArray, minutosArray, segundosArray);
 
-    document.querySelector('strong').innerText = resultadoTotal;
+    document.querySelector('strong').textContent = resultadoTotal;
 
     return false;
 }
