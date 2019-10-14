a47d1ea996fa49421437348c1e41bd5d02605ae6
vitipe <victorcasbas@gmail.com>
2019-10-14 20:28:02 -0300

    Pasando código por beatiful.io
    Se estilizó el código, se cambió .innerText por .textContent y se avanzó en tarea 4 de clase 5

--- tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
+++ tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
@@ -10,39 +10,43 @@
 function calcularDuracion(horasArray, minutosArray, segundosArray) {
     let totalHoras = 0;
     let totalMinutos = 0;
-    let totalSegundos= 0;
-    for (let i = 0; i<horasArray.length; i++) {
+    let totalSegundos = 0;
+    for (let i = 0; i < horasArray.length; i++) {
         if (parseInt(horasArray[i].value)) {
             resultadoHoras = totalHoras += (parseInt(horasArray[i].value));
         }
-    } for (let i = 0; i<minutosArray.length; i++) {
+    }
+    for (let i = 0; i < minutosArray.length; i++) {
         if (parseInt(minutosArray[i].value)) {
             resultadoMinutos = totalMinutos += (parseInt(minutosArray[i].value));
         }
-    } if (resultadoMinutos >= 60) {
-        resultadoHoras += Math.floor(resultadoMinutos/60);
+    }
+    if (resultadoMinutos >= 60) {
+        resultadoHoras += Math.floor(resultadoMinutos / 60);
         resultadoMinutos = resultadoMinutos % 60;
-    } for (let i = 0; i<segundosArray.length; i++) {
+    }
+    for (let i = 0; i < segundosArray.length; i++) {
         if (parseInt(segundosArray[i].value)) {
             resultadoSegundos = totalSegundos += (parseInt(segundosArray[i].value));
         }
-    } if (resultadoSegundos >= 60) {
-            resultadoMinutos += Math.floor(resultadoSegundos/60);
-            resultadoSegundos = resultadoSegundos % 60;
-    } return "Tiempo total: " + resultadoHoras + " horas, " + resultadoMinutos + " minutos, " + resultadoSegundos + " segundos."
+    }
+    if (resultadoSegundos >= 60) {
+        resultadoMinutos += Math.floor(resultadoSegundos / 60);
+        resultadoSegundos = resultadoSegundos % 60;
+    }
+    return "Tiempo total: " + resultadoHoras + " horas, " + resultadoMinutos + " minutos, " + resultadoSegundos + " segundos."
 }
 
 
 const $calculoHoras = document.querySelector('#calculo-tiempo-total');
 
-$calculoHoras.onclick = function(){
+$calculoHoras.onclick = function() {
     let horasArray = document.getElementsByClassName('tiempo-horas');
     let minutosArray = document.getElementsByClassName('tiempo-minutos');
     let segundosArray = document.getElementsByClassName('tiempo-segundos');
     resultadoTotal = calcularDuracion(horasArray, minutosArray, segundosArray);
 
     document.querySelector('strong').innerText = resultadoTotal;
-   
+
     return false;
 }
-
--- tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
+++ tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
@@ -5,3 +5,36 @@
 // 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+
+
+function calcularPromedio (numerosArray){
+    let contador = 0;
+    for (let i= 0; i<numerosArray.length; i++) {
+        contador += numerosArray[i]
+    } return contador / numerosArray.length;
+}
+
+function calcularNumeroMenor (numerosArray) {
+    let numeroMenor = 0;
+    for (let i=0; i<numerosArray.length; i++) {
+        if (numeroMenor<numerosArray[i]){
+            numeroMenor = numerosArray[i];
+        }
+    } return numeroMenor;
+
+}
+
+function calcularNumeroMayor (numerosArray) {
+    let numeroMayor = 0;
+    for (let i = 0; i < numerosArray.length; i++){
+        if (numeroMayor > numerosArray[i]) {
+            numeroMayor = numerosArray [i];
+        }
+    } return numeroMayor
+}
+
+function calcularModa (numerosArray) {
+
+}
+
