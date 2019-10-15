67d88fe5192cc23f48d0e0cc47de17d47a199341
xRollbacks <56001809+xRollbacks@users.noreply.github.com>
2019-10-15 17:45:40 -0300

    Primer tarea de la clase 5 lista para ser corregida.
    Es un primer intento, mucho copy paste y algunas cosas que no entiendo, pero al menos la logica de como funciona todo me va quedando mas clara.
    
    A futuro estaria bueno reescribir el codigo y ver como usar interpolacion de variables en strings

---
+++ tareas/clase-5/primertarea.js
@@ -0,0 +1,41 @@
+//TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+
+/*
+Ejemplo form:
+  <form id="entrada-bar" onsubmit="return false;">
+    <input type="text" placeholder="Ingresá tu nombre" id="nombre-usuario"/>
+    <input type="number" placeholder="Ingresá tu edad" id="edad-usuario" />
+    <input type="submit" value="Ingresar" id="ingresar"/>
+  </form>
+
+  <div id="resultado">Hola</div>
+*/
+
+document.querySelector("#ingresar").onclick = function() {
+	const $nombreUsuario = document.querySelector("#nombre-usuario").value;
+	const $segundoNombreUsuario = document.querySelector(
+		"#segundo-nombre-usuario"
+	).value;
+	const $apellidoUsuario = document.querySelector("#apellido-usuario").value;
+	const $edadUsuario = Number(document.querySelector("#edad-usuario").value);
+	const nodoPagina = document.querySelector("body");
+	const nuevoParrafo = document.createElement("p");
+	const textoParrafo = document.createTextNode(
+		"Tenes " +
+			$edadUsuario +
+			" años, " +
+			$nombreUsuario +
+			" " +
+			$apellidoUsuario
+	);
+	nuevoParrafo.appendChild(textoParrafo);
+	nodoPagina.appendChild(nuevoParrafo);
+	document.querySelector("h1").textContent = "Bienvenido " + $nombreUsuario;
+
+	return false;
+};
