b098800209494b95c8f3872aa8948f1a3fb6461f
vitipe <victorcasbas@gmail.com>
2019-10-15 17:35:41 -0300

    Arreglando cosas de la tarea, falta arreglar moda

--- tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
+++ tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
@@ -46,8 +46,27 @@ function calcularModa (numerosArray) {
         }
       }
       return moda;
+} //ACA LA MODA ESTA MAL, si pones 1,1,1,1,2,2 te da 2. Ver hacer algo con este tipo de objetos { a: 12 }
+/* PROBAR CON ESTA DE ACA ABAJO QUE ONDA
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]; //array
+var mf = 1; //default maximum frequency
+var m = 0;  //counter
+var item;  //to store item with maximum frequency
+for (var i=0; i<arr1.length; i++)    //select element (current element)
+{
+        for (var j=i; j<arr1.length; j++)   //loop through next elements in array to compare calculate frequency of current element
+        {
+                if (arr1[i] == arr1[j])    //see if element occurs again in the array
+                 m++;   //increment counter if it does
+                if (mf<m)   //compare current items frequency with maximum frequency
+                {
+                  mf=m;      //if m>mf store m in mf for upcoming elements
+                  item = arr1[i];   // store the current element.
+                }
+        }
+        m=0;   // make counter 0 for next element.
 }
-
+*/
 
 const $calculoTotal = document.querySelector('#calculo-total');
 
