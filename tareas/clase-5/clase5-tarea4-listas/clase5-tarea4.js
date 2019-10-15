9c73b183a564e7d48c22ea8764702bfb04e56959
vitipe <victorcasbas@gmail.com>
2019-10-14 22:15:16 -0300

    Resolución de la tarea 4 de la clase 5
    Se podría acomodar más igua

--- tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
+++ tareas/clase-5/clase5-tarea4-listas/clase5-tarea4.js
@@ -12,29 +12,61 @@ function calcularPromedio (numerosArray){
     let contador = 0;
     for (let i= 0; i<numerosArray.length; i++) {
         contador += numerosArray[i]
-    } return contador / numerosArray.length;
+    } 
+    return contador / numerosArray.length;
 }
 
 function calcularNumeroMenor (numerosArray) {
     let numeroMenor = 0;
     for (let i=0; i<numerosArray.length; i++) {
-        if (numeroMenor<numerosArray[i]){
+        if (numerosArray[i] < numeroMenor){
             numeroMenor = numerosArray[i];
         }
-    } return numeroMenor;
-
+    } 
+    return numeroMenor;
 }
 
 function calcularNumeroMayor (numerosArray) {
     let numeroMayor = 0;
     for (let i = 0; i < numerosArray.length; i++){
-        if (numeroMayor > numerosArray[i]) {
+        if (numerosArray[i] > numeroMayor) {
             numeroMayor = numerosArray [i];
         }
-    } return numeroMayor
+    } 
+    return numeroMayor
 }
 
 function calcularModa (numerosArray) {
-
+    let moda = 0;
+      for(let i = 0; i < numerosArray.length; i++){
+        for(let j = 0; j < i; j++){
+           if(numerosArray[j] === numerosArray[i]){
+               moda = numerosArray[j];
+           }
+        }
+      }
+      return moda;
 }
 
+
+const $calculoTotal = document.querySelector('#calculo-total');
+
+$calculoTotal.onclick = function(){
+   
+    const numerosLista = document.querySelectorAll("li");
+    const numerosArray = [];
+    for(let i = 0; i < numerosLista.length; i++){
+      numerosArray.push(Number(numerosLista[i].textContent));
+    }
+    let resultadoPromedio = calcularPromedio(numerosArray);
+    let resultadoNumeroMayor = calcularNumeroMayor(numerosArray);
+    let resultadoNumeroMenor = calcularNumeroMenor(numerosArray);
+    let resultadoModa = calcularModa(numerosArray);
+    
+    document.querySelector('#resultado-promedio').textContent = "El promedio es " + resultadoPromedio;
+    document.querySelector('#resultado-minimo').textContent = "El número más pequeño es " + resultadoNumeroMenor;
+    document.querySelector('#resultado-mayor').textContent = "El número más grande es " + resultadoNumeroMayor;
+    document.querySelector('#resultado-moda').textContent = "El número más frecuente es " + resultadoModa;
+
+    return false;
+}
