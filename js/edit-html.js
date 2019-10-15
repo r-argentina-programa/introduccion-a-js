b559693232ebd0afe3ff2823685831732b7fbd83
Felipe Bertocchi <fabertocchi@gmail.com>
2019-10-15 01:42:04 -0300

    Update edit-html.js

--- js/edit-html.js
+++ js/edit-html.js
@@ -9,10 +9,12 @@ console.log(nuestroTitulo.textContent);
 
 const $botonIngreso = document.querySelector('#ingresar') //Se estila usar '$' delante de un elemento que pertenece a html
 
-$botonIngreso.onclick = function () {
+$botonIngreso.onclick = function (evento) {
     const edadUsuario = Number(document.querySelector('#edad-usuario').value);
     let textoResultado;
     (edadUsuario >= 18) ? textoResultado = `Pase nomas` : textoResultado = `No podes pasar`;
 
     document.querySelector('#resultado').innerText = textoResultado;
+    evento.cancel();
 }
+
