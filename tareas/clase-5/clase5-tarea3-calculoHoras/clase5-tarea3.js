150ad90c6cf91e90c8722004cae288b360e216e4
vitipe <victorcasbas@gmail.com>
2019-10-15 18:57:13 -0300

    Arreglando orden de cálculo de duración de videos

--- tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
+++ tareas/clase-5/clase5-tarea3-calculoHoras/clase5-tarea3.js
@@ -7,33 +7,34 @@
 // <strong> pre-creado el tiempo total de los videos.
 
 
-function calcularDuracion(horasArray, minutosArray, segundosArray) {
-    let totalHoras = 0;
-    let totalMinutos = 0;
-    let totalSegundos = 0;
-    for (let i = 0; i < horasArray.length; i++) {
-        if (parseInt(horasArray[i].value)) {
-            resultadoHoras = totalHoras += (parseInt(horasArray[i].value));
-        }
-    }
-    for (let i = 0; i < minutosArray.length; i++) {
-        if (parseInt(minutosArray[i].value)) {
-            resultadoMinutos = totalMinutos += (parseInt(minutosArray[i].value));
-        }
-    }
-    if (resultadoMinutos >= 60) {
-        resultadoHoras += Math.floor(resultadoMinutos / 60);
-        resultadoMinutos = resultadoMinutos % 60;
-    }
+function calcularTiempoTotal(horasArray, minutosArray, segundosArray) {
+    let resultadoSegundos = 0;
+    let resultadoMinutos = 0;
+    let resultadoHoras = 0;
     for (let i = 0; i < segundosArray.length; i++) {
         if (parseInt(segundosArray[i].value)) {
-            resultadoSegundos = totalSegundos += (parseInt(segundosArray[i].value));
+            resultadoSegundos = resultadoSegundos += (parseInt(segundosArray[i].value));
         }
     }
     if (resultadoSegundos >= 60) {
         resultadoMinutos += Math.floor(resultadoSegundos / 60);
         resultadoSegundos = resultadoSegundos % 60;
     }
+    for (let i = 0; i < minutosArray.length; i++) {
+        if (parseInt(minutosArray[i].value)) {
+            resultadoMinutos += (parseInt(minutosArray[i].value));
+        }
+    }
+    if (resultadoMinutos >= 60) {
+        resultadoHoras += Math.floor(resultadoMinutos / 60);
+        resultadoMinutos = resultadoMinutos % 60;
+    }
+    for (let i = 0; i < horasArray.length; i++) {
+        if (parseInt(horasArray[i].value)) {
+            resultadoHoras += (parseInt(horasArray[i].value));
+        }
+    }
+ 
     return "Tiempo total: " + resultadoHoras + " horas, " + resultadoMinutos + " minutos, " + resultadoSegundos + " segundos."
 }
 
