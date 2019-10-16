935a7fdc40bf542eea16f93cbc42453b5c52c935
jmoralesg <johanamgff@gmail.com>
2019-10-16 15:00:03 +1100

    html y css completado

--- tareas/clase-6/tarea-1/tarea1.js
+++ tareas/clase-6/tarea-1/tarea1.js
@@ -1,16 +1,27 @@
-/*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+// TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+// Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+// Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
 
-Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
-*/
+// Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 
+// const $cantidad = document.querySelector('#cantidad-familiares').value;
 
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+// // const $cantidad = document.querySelector('#cantidad-familiares').value;
+// const $calcular = document.querySelector('#calcular');
+// // // const EDADES = $cantidad.value;
 
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
+// $calcular.onclick = function() {
+//   console.log($cantidad);
+//   return $cantidad;
+//   return false;
+//   // console.log(EDADES);
+// };
+
+// // // document.querySelector('#calcular').onclik = function() {
+// // //   const $cantidad = Number(
+// // //     document.querySelector('#cantidad-familiares').value
+// // //   );
+// // //   console.log($cantidad);
+// // //   return false;
+// // // };
+// // console.log($cantidad.value);
