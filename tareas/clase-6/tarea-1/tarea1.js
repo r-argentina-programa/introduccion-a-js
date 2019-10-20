5ca9569d22bf84fbf4ee6a9737c216170d721006
jmoralesg <johanamgff@gmail.com>
2019-10-21 08:04:31 +1100

    solucion a la tarea 1 de la clase 6

--- tareas/clase-6/tarea-1/tarea1.js
+++ tareas/clase-6/tarea-1/tarea1.js
@@ -1,27 +1,84 @@
-// TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-// Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-// Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+// // TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+// // Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+// // Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
 
-// Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
+const $btnAgregar = document.querySelector('#agregar');
+const $btnCalcular = document.querySelector('#calcular');
+const $btnBorrar = document.querySelector('#borrar');
+const $edades = document.querySelector('#edades');
+const $mayorEdad = document.querySelector('#mayor-edad');
+const $menorEdad = document.querySelector('#menor-edad');
+const $promedio = document.querySelector('#promedio');
+let edad = [];
 
-// const $cantidad = document.querySelector('#cantidad-familiares').value;
+$btnAgregar.onclick = function() {
+  function creaEntradas() {
+    const $integrantes = Number(document.querySelector('#integrantes').value);
+    let nuevaEntrada;
 
-// // const $cantidad = document.querySelector('#cantidad-familiares').value;
-// const $calcular = document.querySelector('#calcular');
-// // // const EDADES = $cantidad.value;
+    while ($edades.hasChildNodes()) {
+      $edades.removeChild($edades.lastChild);
+    }
 
-// $calcular.onclick = function() {
-//   console.log($cantidad);
-//   return $cantidad;
-//   return false;
-//   // console.log(EDADES);
-// };
+    for (let i = 0; i < $integrantes; i++) {
+      $edades.appendChild(document.createTextNode('Integrante ' + (i + 1)));
+      nuevaEntrada = document.createElement('input');
+      nuevaEntrada.setAttribute('type', 'number');
+      nuevaEntrada.className = 'entradas';
+      $edades.appendChild(nuevaEntrada);
+    }
+  }
 
-// // // document.querySelector('#calcular').onclik = function() {
-// // //   const $cantidad = Number(
-// // //     document.querySelector('#cantidad-familiares').value
-// // //   );
-// // //   console.log($cantidad);
-// // //   return false;
-// // // };
-// // console.log($cantidad.value);
+  creaEntradas();
+
+  $btnCalcular.onclick = calculaDatos;
+
+  function calculaDatos() {
+    function promedio() {
+      let promedio = 0;
+      let edadesEntradas = document.querySelectorAll('.entradas');
+
+      for (let i = 0; i < edadesEntradas.length; i++) {
+        promedio += Number(edadesEntradas[i].value) / edadesEntradas.length;
+      }
+      $promedio.textContent = promedio;
+    }
+
+    promedio();
+
+    function mayorEdad() {
+      let edadesEntradas = document.querySelectorAll('.entradas');
+
+      let edadMayor = 0;
+
+      for (let i = 0; i < edadesEntradas.length; i++) {
+        //convierte valores del NodeList a numeros
+        edad.push(Number(edadesEntradas[i].value));
+      }
+      for (let i = 0; i < edad.length; i++) {
+        if (edad[i] > edadMayor) {
+          edadMayor = edad[i];
+        }
+      }
+      $mayorEdad.textContent = edadMayor;
+    }
+    mayorEdad();
+
+    function menorEdad() {
+      let edadMenor = edad[1];
+      for (let i = 0; i < edad.length; i++) {
+        if (edad[i] < edadMenor) {
+          edadMenor = edad[i];
+        }
+      }
+      $menorEdad.textContent = edadMenor;
+      console.log(edadMenor);
+    }
+
+    menorEdad();
+  }
+};
+
+$btnBorrar.onclick = function() {
+  location.reload();
+};
