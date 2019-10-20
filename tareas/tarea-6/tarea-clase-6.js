b1fc6bcb8cdebc35174e42c7d1ab1ad35e61b3d8
ClassicalJo <55909151+ClassicalJo@users.noreply.github.com>
2019-10-20 15:25:45 +1100

    Cambia a Kebab-case los ID y correcciones
    En resumen:
    Cambie todos los IDS y clases en el codigo de camelCase a kebab-case. Es decir, todo lo que venga del HTML o que se cree en el HTML tiene kebab-case.
    
    Borre todos los comentarios (que al fin y al cabo eran mas comentarios que solo tenian sentido para mi que para otras personas).
    
    Aclare los nombres de las funciones para que sea mas claro lo que hacen, especialmente aquellas que tienen uso en mas de un array y no solo para un caso particular.
    
    Reduje el spanglish, cambie placeholder y dummys por temp,
    
    Y por ultimo borre las lineas vacias sin razon de ser.

--- tareas/tarea-6/tarea-clase-6.js
+++ tareas/tarea-6/tarea-clase-6.js
@@ -1,34 +1,24 @@
-/*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
-
-Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
-*/
-
-//Crear inputs y labels
-
-function crearEntrada(param) {
-    $formularioGrupoFamiliar = document.querySelector('#formularioGrupoFamiliar')
+function crearEntrada(indice) {
+    $formularioGrupoFamiliar = document.querySelector('#formulario-grupo-familiar')
 
     $label = document.createElement("label")
-    $label.textContent = `Edad del familiar ${param}: `
+    $label.textContent = `Edad del familiar ${indice}: `
 
     $input = document.createElement("input")
     $input.type = "number"
-    $input.className = "inputEdad"
+    $input.className = "input-edad"
 
     $labelSalario = document.createElement("label");
-    $labelSalario.className = "labelSalario"
+    $labelSalario.className = "label-salario"
 
 
-    nuevoParrafo = document.createElement("p")
+    $nuevoParrafo = document.createElement("p")
 
-    nuevoParrafo.appendChild($label)
-    nuevoParrafo.appendChild($input)
-    nuevoParrafo.appendChild($labelSalario)
+    $nuevoParrafo.appendChild($label)
+    $nuevoParrafo.appendChild($input)
+    $nuevoParrafo.appendChild($labelSalario)
 
-    $formularioGrupoFamiliar.appendChild(nuevoParrafo)
+    $formularioGrupoFamiliar.appendChild($nuevoParrafo)
 }
 
 function generarFormulario() {
@@ -39,187 +29,184 @@ function generarFormulario() {
     }
 }
 
-//Recopilacion de datos del formulario en arrays
+let generarArrays = function () {
+    arrayInputEdad = document.querySelectorAll(".input-edad")
+    arrayInputEdad = valoresArrayANumeros(arrayInputEdad)
 
+    $mostrarEdadMayor = document.querySelector("#mayor-edad")
+    $mostrarEdadMayor.value = buscarValorMayor(arrayInputEdad)
 
+    $mostrarEdadMenor = document.querySelector("#menor-edad")
+    $mostrarEdadMenor.value = buscarValorMenor(arrayInputEdad)
 
-generarArrays = function () {
-    arrayInputEdad = document.querySelectorAll(".inputEdad")
-    arrayInputEdad = arrayEnNumericos(arrayInputEdad)
+    $mostrarEdadPromedio = document.querySelector("#promedio-edad")
+    $mostrarEdadPromedio.value = determinarValorPromedio(arrayInputEdad)
 
-    $mostrarEdadMayor = document.querySelector("#mayorEdad")
-    $mostrarEdadMayor.value = calcularMayor(arrayInputEdad)
+    arrayInputSalarios = document.querySelectorAll(".input-salario")
+    if (arrayInputSalarios.length !== 0) {
+        arrayInputSalarios = valoresArrayANumeros(arrayInputSalarios)
 
-    $mostrarEdadMenor = document.querySelector("#menorEdad")
-    $mostrarEdadMenor.value = calcularMenor(arrayInputEdad)
+        $mostrarSalarioMayor = document.querySelector("#salario-anual-mayor")
+        $mostrarSalarioMayor.value = buscarValorMayor(arrayInputSalarios) * 12
 
-    $mostrarEdadPromedio = document.querySelector("#promedioEdad")
-    $mostrarEdadPromedio.value = calcularPromedio(arrayInputEdad)
+        $mostrarSalarioMenor = document.querySelector("#salario-anual-menor")
+        $mostrarSalarioMenor.value = buscarValorMenor(arrayInputSalarios) * 12
 
-    arrayInputSalarios = document.querySelectorAll(".inputSalario")
-    if (arrayInputSalarios.length !== 0){
-        arrayInputSalarios = arrayEnNumericos(arrayInputSalarios)
+        $mostrarSalarioAnualPromedio = document.querySelector("#salario-anual-promedio")
+        $mostrarSalarioAnualPromedio.value = determinarValorPromedio(arrayInputSalarios) * 12
 
-        $mostrarSalarioMayor = document.querySelector("#mayorSalario")
-        $mostrarSalarioMayor.value = calcularMayor(arrayInputSalarios) * 12
-
-        $mostrarSalarioMenor = document.querySelector("#menorSalario")
-        $mostrarSalarioMenor.value = calcularMenor(arrayInputSalarios) * 12
-
-        $mostrarSalarioAnualPromedio = document.querySelector("#salarioAnualPromedio")
-        $mostrarSalarioAnualPromedio.value = calcularPromedio(arrayInputSalarios) * 12
-
-        $mostrarSalarioMensualPromedio = document.querySelector("#salarioMensualPromedio")
-        $mostrarSalarioMensualPromedio.value = calcularPromedio(arrayInputSalarios)
+        $mostrarSalarioMensualPromedio = document.querySelector("#salario-mensual-promedio")
+        $mostrarSalarioMensualPromedio.value = determinarValorPromedio(arrayInputSalarios)
     }
 
 }
 
-//convierte todos los elementos del array en valores numericos
-function arrayEnNumericos(array) {
+function valoresArrayANumeros(array) {
     let i;
-    let placeholder;
-    let dummyArray = [];
+    let temp;
+    let tempArray = [];
     for (i = 0; i < array.length; i++) {
         if (array[i].value !== '') {
-            placeholder = array[i].value
-            dummyArray.push(Number(placeholder))
+            temp = array[i].value
+            tempArray.push(Number(temp))
         }
     }
-    return dummyArray
+    return tempArray
 }
 
-function calcularMayor(array) {
+function buscarValorMayor(array) {
     let i;
-    let placeholder = array[0];
+    let temp = array[0];
     for (i = 0; i < array.length; i++) {
-        array[i] > placeholder ? placeholder = array[i] : ''
+        array[i] > temp ? temp = array[i] : ''
     }
-    return placeholder
+    return temp
 }
 
-function calcularMenor(array) {
+function buscarValorMenor(array) {
     let i;
-    let placeholder = array[0]
+    let temp = array[0]
     for (i = 0; i < array.length; i++) {
-        placeholder < array[i] ? '' : placeholder = array[i]
+        temp < array[i] ? '' : temp = array[i]
     }
-    return placeholder
+    return temp
 }
 
-function calcularPromedio(array) {
+function determinarValorPromedio(array) {
     let i;
-    let placeholder = 0;
+    let temp = 0;
     for (i = 0; i < array.length; i++) {
-        placeholder = placeholder + array[i]
+        temp = temp + array[i]
     }
-    return placeholder / array.length
+    if ((temp % array.length) !== 0) {
+        return (temp / array.length).toFixed(2)
+    }
+    else {
+        return (temp / array.length)
+    }
 }
 
-
-
-clean = function () {
-    $mostrarEdadMayor = document.querySelector("#mayorEdad")
+let limpiarFormulario = function () {
+    $mostrarEdadMayor = document.querySelector("#mayor-edad")
     $mostrarEdadMayor.value = "Mayor edad del grupo"
 
-    $mostrarEdadMenor = document.querySelector("#menorEdad")
+    $mostrarEdadMenor = document.querySelector("#menor-edad")
     $mostrarEdadMenor.value = "Menor edad del grupo"
 
-    $mostrarEdadPromedio = document.querySelector("#promedioEdad")
+    $mostrarEdadPromedio = document.querySelector("#promedio-edad")
     $mostrarEdadPromedio.value = "Promedio edad del grupo"
 
-    $mayorSalario = document.querySelector("#mayorSalario")
-    if ($mayorSalario !== null){
-    $mayorSalario.value = "Mayor salario anual"
+    $salarioAnualMayor = document.querySelector("#salario-anual-mayor")
 
-    $menorSalario = document.querySelector("#menorSalario")
-    $menorSalario.value = "Menor salario anual"
+    if ($salarioAnualMayor !== null) {
+        $salarioAnualMayor.value = "Mayor salario anual"
 
-    $salarioAnualPromedio = document.querySelector("#salarioAnualPromedio")
-    $salarioAnualPromedio.value = "Salario anual promedio"
+        $salarioAnualMenor = document.querySelector("#salario-anual-menor")
+        $salarioAnualMenor.value = "Menor salario anual"
 
-    $salarioMensualPromedio = document.querySelector("#salarioMensualPromedio")
-    $salarioMensualPromedio.value = "Salario mensual promedio"
+        $salarioAnualPromedio = document.querySelector("#salario-anual-promedio")
+        $salarioAnualPromedio.value = "Salario anual promedio"
+
+        $salarioMensualPromedio = document.querySelector("#salario-mensual-promedio")
+        $salarioMensualPromedio.value = "Salario mensual promedio"
     }
-    else {}
 }
 
-clear = function () {
+let empezarDeNuevo = function () {
     $formularioGrupoFamiliar.innerHTML = ''
 
-    $mostrarEdadMayor = document.querySelector("#mayorEdad")
+    $mostrarEdadMayor = document.querySelector("#mayor-edad")
     $mostrarEdadMayor.value = "Mayor edad del grupo"
 
-    $mostrarEdadMenor = document.querySelector("#menorEdad")
+    $mostrarEdadMenor = document.querySelector("#menor-edad")
     $mostrarEdadMenor.value = "Menor edad del grupo"
 
-    $mostrarEdadPromedio = document.querySelector("#promedioEdad")
+    $mostrarEdadPromedio = document.querySelector("#promedio-edad")
     $mostrarEdadPromedio.value = "Promedio edad del grupo"
 
-    $parrafoBotonSalarios = document.querySelector("#parrafoBotonSalarios")
+    $parrafoBotonSalarios = document.querySelector("#parrafo-boton-salarios")
     $parrafoBotonSalarios.removeChild($parrafoExtra)
 
     $botonSalarios.disabled = false
     generarFormulario()
 }
 
-//Agregamos los salarios
-agregarCalculoSalarios = function(){
-    $labelSalario = document.querySelectorAll(".labelSalario")
+agregarCalculoSalarios = function () {
+    $labelSalario = document.querySelectorAll(".label-salario")
     agregaLabels($labelSalario)
     agregaSalarios($labelSalario)
     agregaCalculos()
 }
 
-function agregaLabels(array){
+function agregaLabels(array) {
     let i;
-    for (i = 0; i < array.length; i++){
+    for (i = 0; i < array.length; i++) {
         array[i].textContent = "  Salario mensual: "
     }
-    
+
 }
 
-function agregaSalarios(array){
+function agregaSalarios(array) {
     let i;
-    for (i = 0; i < array.length; i++){
+    for (i = 0; i < array.length; i++) {
         $inputSalario = document.createElement("input")
         $inputSalario.type = "number"
-        $inputSalario.className = "inputSalario"
-    
+        $inputSalario.className = "input-salario"
+
         array[i].appendChild($inputSalario)
     }
 }
 
-function agregaCalculos(){
-    $mayorSalario = document.createElement("input")
-    $mayorSalario.type = "text"
-    $mayorSalario.id = "mayorSalario"
-    $mayorSalario.disabled = "true"
-    $mayorSalario.value = "Mayor salario anual"
+function agregaCalculos() {
+    $salarioAnualMayor = document.createElement("input")
+    $salarioAnualMayor.type = "text"
+    $salarioAnualMayor.id = "salario-anual-mayor"
+    $salarioAnualMayor.disabled = "true"
+    $salarioAnualMayor.value = "Mayor salario anual"
 
-    $menorSalario = document.createElement("input");
-    $menorSalario.type = "text"
-    $menorSalario.id = "menorSalario"
-    $menorSalario.disabled = "true"
-    $menorSalario.value = "Menor salario anual"
+    $salarioAnualMenor = document.createElement("input");
+    $salarioAnualMenor.type = "text"
+    $salarioAnualMenor.id = "salario-anual-menor"
+    $salarioAnualMenor.disabled = "true"
+    $salarioAnualMenor.value = "Menor salario anual"
 
     $salarioAnualPromedio = document.createElement("input");
     $salarioAnualPromedio.type = "text"
-    $salarioAnualPromedio.id = "salarioAnualPromedio"
+    $salarioAnualPromedio.id = "salario-anual-promedio"
     $salarioAnualPromedio.disabled = "true"
     $salarioAnualPromedio.value = "Salario anual promedio"
 
     $salarioMensualPromedio = document.createElement("input");
     $salarioMensualPromedio.type = "text"
-    $salarioMensualPromedio.id = "salarioMensualPromedio"
+    $salarioMensualPromedio.id = "salario-mensual-promedio"
     $salarioMensualPromedio.disabled = "true"
     $salarioMensualPromedio.value = "Salario mensual promedio"
 
     $parrafoExtra = document.createElement("p")
-    $parrafoBotonSalarios = document.querySelector("#parrafoBotonSalarios")
+    $parrafoBotonSalarios = document.querySelector("#parrafo-boton-salarios")
 
-    $parrafoExtra.appendChild($mayorSalario)
-    $parrafoExtra.appendChild($menorSalario)
+    $parrafoExtra.appendChild($salarioAnualMayor)
+    $parrafoExtra.appendChild($salarioAnualMenor)
     $parrafoExtra.appendChild($salarioAnualPromedio)
     $parrafoExtra.appendChild($salarioMensualPromedio)
 
@@ -231,35 +218,13 @@ function agregaCalculos(){
 $botonCalcular = document.querySelector("#calcular")
 $botonCalcular.onclick = generarArrays
 
-$botonClean = document.querySelector("#botonClean")
-$botonClean.onclick = clean
+$botonLimpiarFormulario = document.querySelector("#boton-limpiar-formulario")
+$botonLimpiarFormulario.onclick = limpiarFormulario
 
-$botonClear = document.querySelector("#botonClear")
-$botonClear.onclick = clear
+$botonEmpezarDeNuevo = document.querySelector("#boton-empezar-de-nuevo")
+$botonEmpezarDeNuevo.onclick = empezarDeNuevo
 
-$botonSalarios = document.querySelector("#botonSalarios")
+$botonSalarios = document.querySelector("#boton-salarios")
 $botonSalarios.onclick = agregarCalculoSalarios
 
-
-
-
-
-
 generarFormulario()
-
-
-
-
-
-
-
-
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante
-de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, 
-salario anual promedio y salario mensual promedio.
-
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
