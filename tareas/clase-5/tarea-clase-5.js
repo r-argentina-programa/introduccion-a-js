cf9997db78e82a1fe7fdeb141b99566dff23e5b7
JoaquinR <55899656+JoaquinR98@users.noreply.github.com>
2019-10-23 10:40:34 -0300

    Hace formulario de salario mensual

--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -17,11 +17,28 @@ $botonCalcular.onclick = function()
 }
 */
 
+	console.log('Tarea 5');
+
+
 //TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
 //TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
+	function calcularSalarioMensual (salarioAnual){
+		return salarioAnual / 12;
+	}
+
+	const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual')
+
+	$calcularSalarioMensual.onclick = function(){
+		const salarioAnual = Number(document.querySelector('#salario-anual').value);
+		const salarioMensual = calcularSalarioMensual (salarioAnual);
+		document.querySelector('#salario-mensual').value = salarioMensual;
+		
+		return false;
+	}
+	
 //TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
 // creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
 // también vamos a crear un <h1> que diga Bienvenido!
