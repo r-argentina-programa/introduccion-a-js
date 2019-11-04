1e74295e9a74fa888892f11ca5b504854e7bec24
JoaquinR <55899656+JoaquinR98@users.noreply.github.com>
2019-11-04 18:56:37 -0300

    hace cambios

--- tareas/clase-5/mi-tarea-clase-5.js
+++ tareas/clase-5/mi-tarea-clase-5.js
@@ -2,16 +2,17 @@ console.log('hola');
 console.log(document.querySelector('h1').textContent);
 
 
-const $datosFinales = document.querySelector('#entrar');
+const $entrar = document.querySelector('#entrar');
 
-$datosFinales.onclick = function(){
-	const primerNombre = document.querySelector('#primer-nombre').textContent;
-	const segundoNombre = document.querySelector('#segundo-nombre').textContent;
-	const apellido = document.querySelector('#apellido').textContent;
+$entrar.onclick = function(){
+	const primerNombre = document.querySelector('#primer-nombre').value;
+	const segundoNombre = document.querySelector('#segundo-nombre').value;
+	const apellido = document.querySelector('#apellido').value;
 	const edad = Number(document.querySelector('#edad').value);
 
 
-	document.querySelector('#datos-finales').value = $datosFinales;
+	document.querySelector('#datos-finales').value = `Tu nombre es ${primerNombre}, tu segundo nombre es ${segundoNombre},
+	tu apellido es ${apellido} y tu edad es ${edad}`;
 
 	return false;
 }
