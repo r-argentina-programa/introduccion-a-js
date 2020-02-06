7fed6deb7cd85783df187e90a37b6df54a74caa8
Santiago Hadad <santihadad.sh@hotmail.com>
2020-02-05 22:10:46 -0300

    Reolvi la primer tarea
    Se escribio el script para la primer tarea junto con el nuevo archivo html

--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -17,21 +17,25 @@ $botonCalcular.onclick = function()
 }
 */
 
-//Probando el GitHUb
-
-alert ('Como te llamas') //Alerta para ver si el HTML me toma correctamente el script (src)
-
 //TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
 //TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
+function calcularSalarioMensual(salarioMensual){
+  const mesesAnio = 12;
+  return salarioMensual/mesesAnio;
+}
 
+const $calcularSalarioMensual =  document.querySelector('#calcular-salario-mensual')
 
-
-
-
-
+$calcularSalarioMensual.onclick = function(){
+  const salarioAnual = Number(document.querySelector('#salario-anual').value)
+  const salarioMensual = calcularSalarioMensual(salarioAnual)
+  document.querySelector('#salario-mensual').value = salarioMensual
+  
+  return false
+}
 
 
 
