8ceb654490a41d84e85eb70347227a9282492ec1
MarcosJinno <marcos.jinno@gmail.com>
2020-03-19 18:06:02 -0300

    v6
    Se corrigieron las funciones mostrarMayor, mostrarMenor y mostrarPromedio para que al clickear sobre el botón "Limpiar", borre los valores ingresados  antes

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -31,21 +31,24 @@ document.querySelector('#calcular').onclick = function () {
     mostrarMayor(calculos,calcularMayorEdad);
     mostrarMenor(calculos,calcularMenorEdad);
     mostrarPromedio(calculos,calcularPromEdad);
+    mostrarMayor("mayor",calcularMayorEdad(edades));
+    mostrarMenor("menor",calcularMenorEdad(edades));
+    mostrarPromedio("promedio",calcularPromEdad(edades));
     mostrarResultados();
     
    
     
     //calculos es un NodeList con los párrafos
-    function mostrarMayor(calculos,calcularMayorEdad) { 
-        calculos[0].textContent += calcularMayorEdad(edades);
+    function mostrarMayor(texto,valor) { 
+        document.querySelector(`#${texto}-edad`).textContent = valor;
     }
 
-    function mostrarMenor(calculos,calcularMenorEdad){
-        calculos[1].textContent += calcularMenorEdad(edades);
+    function mostrarMenor(texto,valor){
+        document.querySelector(`#${texto}-edad`).textContent = valor;
     }
 
-    function mostrarPromedio(calculos,calcularPromEdad){
-        calculos[2].textContent += calcularPromEdad(edades);
+    function mostrarPromedio(texto,valor){
+        document.querySelector(`#${texto}-edad`).textContent = valor;
     }
  
     event.preventDefault();
