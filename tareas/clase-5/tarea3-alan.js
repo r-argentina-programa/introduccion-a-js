64502026b9dc754937b795cf7f5a1ac4bd0a15c0
Alan <alan.navarro.24895@gmail.com>
2020-07-25 00:56:11 -0300

    update tarea 3

--- tareas/clase-5/tarea3-alan.js
+++ tareas/clase-5/tarea3-alan.js
@@ -6,3 +6,33 @@
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
 
+
+
+
+
+function convertirSegundosAMinutos($totalSeg){
+   const resultadoMin = $totalSeg / 60;
+   const resultadoSeg = $totalSeg % 60;
+   return resultadoMin,resultadoSeg;
+}
+
+document.querySelector("#boton-calcular").onclick = function(){
+
+let $segIngresados = document.querySelectorAll(".segundos-ingresados");
+const $minIngresados = document.querySelectorAll(".minutos-ingresados");
+const $hrsIngresadas = document.querySelectorAll(".horas-ingresadas");
+    
+let totalSeg;
+let totalMin;
+let totalHrs;
+
+for(i = 0 ; i<$segIngresados.length ; i++){
+    totalSeg = Number(totalSeg) + Number($segIngresados[i]).value;
+}
+
+
+        console.log(totalSeg);
+       
+    //console.log(convertirSegundosAMinutos($totalMin));
+}
+
