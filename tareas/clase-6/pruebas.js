2cf7d8f9403b3218767bcc960b17bb34efb4262d
Javier P <javier.aleparedes@gmail.com>
2020-10-12 19:09:53 -0300

    Agregando linea vacia al final del HTML

---
+++ tareas/clase-6/pruebas.js
@@ -0,0 +1,7 @@
+function probarValidarFamiliares() {
+    console.assert(
+        validarFamiliares(0) === 'Debe ingresar por lo menos 1 integrante en la familia',
+        'Validar familiares no validó que se haya ingresado por lo menos 1 integrante en la familia'
+    )
+
+}
