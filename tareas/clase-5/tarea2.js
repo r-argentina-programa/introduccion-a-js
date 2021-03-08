10e0d8ef5a39ca39315c0ba43d836fda59f863ef
Federico Izaguirre <fedej.izaguirre@gmail.com>
2021-03-08 21:14:46 +0100

    añadir tarea2 y tarea3.js
    Se necesita más luego estos ejercicios.

---
+++ tareas/clase-5/tarea2.js
@@ -0,0 +1,7 @@
+//TAREA: En otro archivo distinto,
+// Por cada clase de r/argentina programa existente, vamos a pedir:
+// horas, minutos y segundos de cada video. Ej. Si un video dura
+// 2 horas, 38 minutos y 20 segundos, vamos a rellenar 3 campos de texto con
+// cada dato.
+// al apretar el botón "Calcular tiempo total", debe mostrar en un
+// <strong> pre-creado el tiempo total de los videos.
