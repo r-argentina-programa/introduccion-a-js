af1317a1a863a219c67bac4e513011f55ef87769
Federico Izaguirre <fedej.izaguirre@gmail.com>
2021-03-19 10:43:45 +0100

    subir tarea2.js
    Prueba de resultados por consola. Falta manipular el DOM para mostrar el resultado.

--- tareas/clase-5/tarea2.js
+++ tareas/clase-5/tarea2.js
@@ -5,3 +5,82 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+
+const $botonCalcularTiempo = document.querySelector("#calcular-tiempo");
+
+
+$botonCalcularTiempo.onclick = function() {
+
+    // carga de arrays con todos los inputs.
+
+    let arraySegundos = [];
+    let $arraySegundos = document.getElementsByClassName("segundos");
+
+    for (i=0; i<$arraySegundos.length; i++) {
+        let campoSegundos = $arraySegundos[i];
+        arraySegundos.push(Number(campoSegundos.value));
+    }
+
+    let arrayMinutos = [];
+    let $arrayMinutos = document.getElementsByClassName("minutos");
+
+    for (i=0; i<$arraySegundos.length; i++) {
+        let campoMinutos = $arraySegundos[i];
+        arrayMinutos.push(Number(campoMinutos.value));
+    }
+
+    let arrayHoras = [];
+    let $arrayHoras = document.getElementsByClassName("horas");
+
+    for (i=0; i<$arrayHoras.length; i++) {
+        let campoHoras = $arrayHoras[i];
+        arrayHoras.push(Number(campoHoras.value));
+    }
+
+    // cálculo de tiempo total
+
+    let totalHoras = 0;
+    for (i=0; i<arrayHoras.length; i++) {
+        totalHoras+= arrayHoras[i];
+    }
+
+    let totalMinutos = 0;
+    for (i=0; i<arrayMinutos.length; i++) {
+        totalMinutos+= arrayMinutos[i];
+    }
+
+    let totalSegundos = 0;
+    for (i=0; i<arrayMinutos.length; i++) {
+        totalSegundos+= arraySegundos[i];
+    }
+
+    // transformar resultados totales
+
+    let divisionSegundos, enteroSegundos, decimalSegundos;
+    let divisionMinutos, enteroMinutos, decimalMinutos;
+    let segundosActual, minutosActual, horasActual;
+
+    if (totalSegundos >= 60) {
+        divisionSegundos = totalSegundos / 60;
+        enteroSegundos = Math.floor(divisionSegundos);
+        decimalSegundos = divisionSegundos - enteroSegundos;
+        segundosActual = Math.round(decimalSegundos * 60);
+    } else {
+        segundosActual = Number(totalSegundos);
+    }
+
+    if (totalMinutos >= 60) {
+        divisionMinutos = totalMinutos / 60;
+        enteroMinutos = Math.floor(divisionMinutos);
+        decimalMinutos = divisionMinutos - enteroMinutos;
+        minutosActual = Math.round(decimalMinutos * 60);
+    } else {
+        minutosActual = Number(totalMinutos);
+    }
+
+    horasActual = totalHoras + enteroMinutos;
+
+    console.log(horasActual);
+    console.log(minutosActual);
+    console.log(segundosActual);
+}
