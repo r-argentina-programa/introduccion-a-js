f9e5d586eff3e35e754a4f9b3ffd13dfa942b32f
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-03-31 17:08:21 -0300

    hace tarea

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -6,7 +6,85 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+console.log('hola');
 
+const $botonInicial = document.querySelector('#boton-inicial');
+
+$botonInicial.onclick = function() {
+    const $divFamilia = document.querySelector('div.familia');
+    $divFamilia.innerHTML = '';
+    const cantidadFamilia = Number(document.querySelector('#cantidad-familia').value);
+
+    for (let i = 0; i < cantidadFamilia; i++) {
+        const $nuevoLabel = document.createElement('label');
+        $nuevoLabel.innerHTML = `Edad integrante ${i+1}`;
+
+        const $nuevoInput = document.createElement('input');
+        $nuevoInput.classList.add('edades');
+
+        $divFamilia.appendChild(document.createElement('br'));
+
+        $divFamilia.appendChild($nuevoLabel);
+        $divFamilia.appendChild(document.createElement('br'));
+        $divFamilia.appendChild($nuevoInput);
+        $divFamilia.appendChild(document.createElement('br'));
+        
+    }
+
+    
+    $divFamilia.appendChild(document.createElement('br'));
+    const $divCalculos = document.querySelector('#calculos');
+    $divCalculos.style.display = '';
+
+    document.querySelector('#boton-calculos').onclick = function() {
+
+        const $nodeEdades = document.querySelectorAll('.edades');
+        const arrayEdades = [];
+        for (let i = 0; i < $nodeEdades.length; i++) {
+            arrayEdades.push(Number($nodeEdades[i].value))
+        }
+
+
+        const $textoResultado = document.createTextNode(`La edad mas chica es ${numeroMinimo(arrayEdades)}, la edad mas grande es ${numeroMaximo(arrayEdades)} y la edad promedio es ${calcularPromedio(arrayEdades)}`)
+
+        $divFamilia.appendChild($textoResultado);
+
+    }
+    return false;
+}
+
+
+
+
+function calcularPromedio(array) {
+    let contador = 0;
+    for (let i = 0; i < array.length; i++) {
+        contador += array[i];
+    }
+    return contador / array.length;
+}
+
+function numeroMaximo(array) {
+    let numMayor = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > numMayor) {
+            numMayor = array[i];
+        } 
+    }
+
+    return numMayor;
+}
+
+function numeroMinimo(array) {
+    let numMenor = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < numMenor) {
+            numMenor = array[i];
+        } 
+    }
+
+    return numMenor;
+}
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
