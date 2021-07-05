bc660d0b477363bc156950be475b2dbb08f8a00b
BrenMagallanes <bren.magallanes05@gmail.com>
2021-07-04 21:06:17 -0300

    Agrega una funcion que calcula el salario mensual

--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -18,9 +18,15 @@ $botonCalcular.onclick = function()
 */
 
 //TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
+
 //TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+ 
+function calcularSalarioMensual(salarioAnual){
+  return (salarioAnual / 12)
+}
+
 
 //TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
 // creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
