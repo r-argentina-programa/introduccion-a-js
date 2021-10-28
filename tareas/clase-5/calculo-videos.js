4bdec3dee57d5a8412e82ee250d98ca960b5168b
FrancoPineda <pfranco998@gmail.com>
2021-10-28 01:21:46 -0300

    Subir tarea clase 5
    se sube por primera vez a github la tarea. Clase 5

---
+++ tareas/clase-5/calculo-videos.js
@@ -0,0 +1,27 @@
+const $botonCalcular = document.querySelector("#boton-calcular");
+const $horasPorClase = document.querySelectorAll(".horas");
+const $minutosPorClase = document.querySelectorAll(".minutos");
+const $segundosPorClase = document.querySelectorAll(".segundos");
+const $textoResultadoTotal = document.querySelector("#resultado");
+
+
+let horasTotalesDeClase = 0;
+let minutosTotalesDeClase = 0;
+let segundosTotalesDeClase = 0;
+
+
+$botonCalcular.onclick = function () {
+    for (let i = 0; i < $horasPorClase.length; i++) {
+        horasTotalesDeClase = horasTotalesDeClase + $horasPorClase[i].value;
+    }
+
+    for (let i = 0; i < $minutosPorClase.length; i++) {
+        minutosTotalesDeClase = minutosTotalesDeClase + $minutosPorClase[i].value;
+    }
+
+    for (let i = 0; i < $segundosPorClase.length; i++) {
+        segundosTotalesDeClase = segundosTotalesDeClase + $segundosPorClase[i].value;
+    }
+
+    $textoResultadoTotal.textContent = `El tiempo total de clase es de ${horasTotalesDeClase} horas, ${minutosTotalesDeClase} minutos y ${segundosTotalesDeClase} segundos.`;
+}
