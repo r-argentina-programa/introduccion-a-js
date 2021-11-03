325422b4e5bae97d6c5fe860713d0907d767f71d
FrancoPineda <pfranco998@gmail.com>
2021-11-02 21:56:36 -0300

    Agrega bug en que se sumaban strings en vez de Number

--- tareas/clase-5/calculo-videos.js
+++ tareas/clase-5/calculo-videos.js
@@ -1,11 +1,12 @@
+const $botonCalcular = document.querySelector("#boton-calcular");
+const $horasPorClase = document.querySelectorAll(".horas");
+const $minutosPorClase = document.querySelectorAll(".minutos");
+const $segundosPorClase = document.querySelectorAll(".segundos");
+const $textoResultadoTotal = document.querySelector("#resultado");
+
+
+
 $botonCalcular.onclick = function () {
-    const $botonCalcular = document.querySelector("#boton-calcular");
-    const $horasPorClase = document.querySelectorAll(".horas");
-    const $minutosPorClase = document.querySelectorAll(".minutos");
-    const $segundosPorClase = document.querySelectorAll(".segundos");
-    const $textoResultadoTotal = document.querySelector("#resultado");
-
-
     let horasTotalesDeClase = 0;
     let minutosTotalesDeClase = 0;
     let segundosTotalesDeClase = 0;
@@ -13,15 +14,15 @@ $botonCalcular.onclick = function () {
 
 
     for (let i = 0; i < $horasPorClase.length; i++) {
-        horasTotalesDeClase = horasTotalesDeClase + $horasPorClase[i].value;
+        horasTotalesDeClase += Number($horasPorClase[i].value);
     }
 
     for (let i = 0; i < $minutosPorClase.length; i++) {
-        minutosTotalesDeClase = minutosTotalesDeClase + $minutosPorClase[i].value;
+        minutosTotalesDeClase += Number($minutosPorClase[i].value);
     }
 
     for (let i = 0; i < $segundosPorClase.length; i++) {
-        segundosTotalesDeClase = segundosTotalesDeClase + $segundosPorClase[i].value;
+        segundosTotalesDeClase += Number($segundosPorClase[i].value);
     }
 
     $textoResultadoTotal.textContent = `El tiempo total de clase es de ${horasTotalesDeClase} horas, ${minutosTotalesDeClase} minutos y ${segundosTotalesDeClase} segundos.`;
