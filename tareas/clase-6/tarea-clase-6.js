884e8f10e999ded3098457224d721151571db6e9
FrancoPineda <pfranco998@gmail.com>
2021-11-04 22:55:56 -0300

    Update tarea-clase-6.js

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -5,63 +5,99 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
-document.querySelector("#siguiente-paso").onclick = function() {
-    const $cantidadIntegrantes = document.querySelector("#cantidad-integrantes");
+
+document.querySelector('#siguiente-paso').onclick = function(event) {
+    const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
     const cantidadIntegrantes = Number($cantidadIntegrantes.value);
-
+  
     borrarIntegrantesAnteriores();
-    crearIntegrante(cantidadIntegrantes);
-
+    crearIntegrantes(cantidadIntegrantes);
+  
     event.preventDefault();
-};
-
-document.querySelector("#calcular").onclick = function(event) {
+  };
+  
+  document.querySelector('#calcular').onclick = function(event) {
     const numeros = obtenerEdadesIntegrantes();
-    mostrarEdad('mayor', calcularMayorNumero(numeros));
-    mostrarEdad('menor', calcularMenorNumero(numeros));
-    mostrarEdad('promedio', calcularPromedio(numeros));
+    mostrarEdad('mayor', obtenerMayorNumero(numeros));
+    mostrarEdad('menor', obtenerMenorNumero(numeros));
+    mostrarEdad('promedio', obtenerPromedio(numeros));
     mostrarResultados();
-
+  
     event.preventDefault();
-};
-
-document.querySelector("#resetear").onclick = resetear;
-
-function borrarIntegrantesAnteriores() {
-    const $integrantes = document.querySelectorAll(".integrante");
-    for (let i=0; i< $integrantes.length; i++) {
-        $integrantes[i].remove();
+  };
+  
+  document.querySelector('#resetear').onclick = resetear;
+  
+  function borrarIntegrantesAnteriores() {
+    const $integrantes = document.querySelectorAll('.integrante');
+    for (let i = 0; i < $integrantes.length; i++) {
+      $integrantes[i].remove();
     }
-}
-
-function crearIntegrantes(cantidadIntegrantes) {
-
+  }
+  
+  function crearIntegrantes(cantidadIntegrantes) {
+  
     if (cantidadIntegrantes > 0) {
-        mostrarBotonCalculo();
+      mostrarBotonCalculo();
     } else {
-        resetear();
+      resetear();
     }
-
+  
     for (let i = 0; i < cantidadIntegrantes; i++) {
-        crearIntegrante(i);
+      crearIntegrante(i);
     }
-}
-
-function crearIntegrante(indice) {
-    const $div = document.createElement("div");
+  }
+  
+  function crearIntegrante(indice) {
+    const $div = document.createElement('div');
     $div.className = 'integrante';
-    
-    const $label = document.createElement("label");
-    $label.textContent = "Edad del integrante #: " + (indice + 1);
-    const $input = document.createElement("input");
-    $input.type = "number";
-
+  
+    const $label = document.createElement('label');
+    $label.textContent = 'Edad del integrante #: ' + (indice + 1);
+    const $input = document.createElement('input');
+    $input.type = 'number';
+  
     $div.appendChild($label);
     $div.appendChild($input);
-
-    const $integrantes = document.querySelector("#integrantes");
+  
+    const $integrantes = document.querySelector('#integrantes');
     $integrantes.appendChild($div);
-}
+  }
+  
+  function resetear() {
+    borrarIntegrantesAnteriores();
+    ocultarBotonCalculo();
+    ocultarResultados();
+  }
+  
+  function ocultarBotonCalculo() {
+    document.querySelector('#calcular').className = 'oculto';
+  }
+  
+  function mostrarBotonCalculo() {
+    document.querySelector('#calcular').className = '';
+  }
+  
+  function ocultarResultados() {
+    document.querySelector('#analisis').className = 'oculto';
+  }
+  
+  function mostrarResultados() {
+    document.querySelector('#analisis').className = '';
+  }
+  
+  function mostrarEdad(tipo, valor) {
+    document.querySelector(`#${tipo}-edad`).textContent = valor;
+  }
+  
+  function obtenerEdadesIntegrantes() {
+    const $integrantes = document.querySelectorAll('.integrante input');
+    const edades = [];
+    for (let i = 0; i < $integrantes.length; i++) {
+      edades.push(Number($integrantes[i].value));
+    }
+    return edades;
+  }
 
 /*
 TAREA:
