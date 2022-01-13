9431c6bf893bf8d01c246535dc8b47484bb058b3
Lean-Lopez <leandrolopezsanm@gmail.com>
2022-01-13 10:42:14 -0300

    Corrigo errores en nombre de variables
    Saco el $ de las variables ya que no hacen referencia a un elemento completo sino al valor de los mismos

--- tareas/clase-5/tarea2.js
+++ tareas/clase-5/tarea2.js
@@ -3,18 +3,18 @@ const $saludoUsuario = document.querySelector("#ingreso-usuario");
 $saludoUsuario.onclick = function() {
 
     const $titulo = document.querySelector("h1");
-    const $nombreUsuario = document.querySelector("#nombre-usuario").value;
-    const $apellidoUsuario = document.querySelector("#apellido-usuario").value;
-    const $edadUsuario = Number(document.querySelector("#edad-usuario").value);
+    const nombreUsuario = document.querySelector("#nombre-usuario").value;
+    const apellidoUsuario = document.querySelector("#apellido-usuario").value;
+    const edadUsuario = Number(document.querySelector("#edad-usuario").value);
 
-    if(chequearEdad($edadUsuario)){
-        $titulo.textContent = `Bienvenido al bar ${$nombreUsuario}`;
+    if(chequearEdad(edadUsuario)){
+        $titulo.textContent = `Bienvenido al bar ${nombreUsuario}`;
     }else{
-        $titulo.textContent = `No podes ingresar al bar ${$nombreUsuario}`;
+        $titulo.textContent = `No podes ingresar al bar ${nombreUsuario}`;
     }
 
     const $textoResultado = document.querySelector("#resultado");
-    $textoResultado.textContent = `Datos del usuario: ${$nombreUsuario} ${$apellidoUsuario} edad = ${$edadUsuario}`;
+    $textoResultado.textContent = `Datos del usuario: ${nombreUsuario} ${apellidoUsuario} edad = ${edadUsuario}`;
     $textoResultado.style.display = "block";
 
     return false;
