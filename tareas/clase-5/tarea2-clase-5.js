e881a89e9fde631d92ed926d4e624f54e12bdcaa
Enzo Juan Andrés Almada <99050038+Lenz-or@users.noreply.github.com>
2022-02-07 12:29:47 -0300

    Cambia y arregla la tarea 5
    Agrega espacios al formulario y luego se hace un cambio en el codigo de JS siguiendo las refrencias de la tarea

--- tareas/clase-5/tarea2-clase-5.js
+++ tareas/clase-5/tarea2-clase-5.js
@@ -7,34 +7,39 @@
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
 
-document.querySelector("#calcular-tiempo-total").onclick = function(){
-    const $hora = Number(document.querySelector("#hora").value);
-    const $minutos = Number(document.querySelector("#minutos").value);
-    const $segundos = Number(document.querySelector("#segundos").value);
-    document.querySelector("#calcular-hora").innerText = calcularHoraTotal($hora,$minutos,$segundos);
-    document.querySelector("#calcular-minutos").innerText = calcularMinutosTotal($hora,$minutos,$segundos);
-    document.querySelector("#calcular-segundos").innerText = calcularSegundosTotal($hora,$minutos,$segundos);
-  
-    return false;
-  }
-  
-  function calcularHoraTotal(hora,minutos,segundos){
-     let horaCalculada = hora;
-     let minutosCalculada = minutos / 60;
-     let segundosCalculada = segundos / 3600;
-     return horaCalculada + minutosCalculada + segundosCalculada;
-  }
-  function calcularMinutosTotal(hora,minutos,segundos){
-    let horaCalculada = hora * 60;
-    let minutosCalculada = minutos;
-    let segundosCalculada = segundos / 60;
-    return horaCalculada + minutosCalculada + segundosCalculada;
-  }
-  function calcularSegundosTotal(hora,minutos,segundos){
-    let horaCalculada = hora * 3600;
-    let minutosCalculada = minutos * 60;
-    let segundosCalculada = segundos;
-    return horaCalculada + minutosCalculada + segundosCalculada;
-  }
-  
-  
+ document.querySelector("#calcular-tiempo-total").onclick= function(){
+   let hora = document.querySelectorAll(".hora");
+   let minutos = document.querySelectorAll(".minutos");
+   let segundos = document.querySelectorAll(".segundos");
+   let horaResultado = 0;
+   let minutosResultado= 0;
+   let segundosResultado= 0;
+
+    for(let i=0;i < hora.length;i++){
+      horaResultado += Number(hora[i].value);
+    }
+    for(let i=0;i < minutos.length;i++){
+      
+      if(minutosResultado < 60){
+        minutosResultado += Number(minutos[i].value);
+      }else{
+        horaResultado += 1;
+        minutosResultado += Number(minutos[i].value);
+        minutosResultado -= 60;
+      }
+    }
+    for(let i=0;i < segundos.length;i++){
+     
+      if(segundosResultado < 60){
+        segundosResultado += Number(segundos[i].value);
+        
+    }else{
+        minutosResultado += 1;
+        segundosResultado += Number(segundos[i].value);
+        segundosResultado -= 60;
+    }
+    }
+
+    document.querySelector("#resultado-tiempo-total").innerText = `El tiempo total de las clases es : ${horaResultado} : ${minutosResultado} : ${segundosResultado}`;
+
+ }
