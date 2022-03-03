5416a2fd4a016ad94212434bedcc5627df63024b
juanmartin7 <93958444+juanmartin7@users.noreply.github.com>
2022-03-02 21:58:05 -0300

    agrega ejercicio 1 de la tarea de la clase 5

---
+++ tareas-mio/clase-5/ejercicio-1/ejercicio-1.js
@@ -0,0 +1,16 @@
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
+const $botonCalcular = document.querySelector('#calcular');
+
+$botonCalcular.onclick = function () {
+  const $salarioAnual = document.querySelector('#salario-anual').value;
+  const MESES_EN_EL_ANIO = 12;
+
+  const salarioMensual = $salarioAnual / MESES_EN_EL_ANIO;
+
+  document.querySelector('#salario-mensual').value = salarioMensual;
+
+  return false;
+};
