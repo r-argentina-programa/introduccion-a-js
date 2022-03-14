4495cbc669273289e51e41c824c7f5a7f66c06eb
juanmartin7 <93958444+juanmartin7@users.noreply.github.com>
2022-03-14 15:28:38 -0300

    Arregla error en índice del array

--- tareas-mio/clase-5/ejercicio-4/ejercicio-4.js
+++ tareas-mio/clase-5/ejercicio-4/ejercicio-4.js
@@ -62,7 +62,7 @@ function obtenerNumeroFrecuente(array) {
   for (let i = 0; i < array.length; i++) {
     let cantidadDeRepeteciones = 0;
     for (let j = 0; j < array.length; j++) {
-      if (array[j] === array[0]) {
+      if (array[j] === array[i]) {
         cantidadDeRepeteciones++;
       }
       if (cantidadDeRepeteciones > contador) {
