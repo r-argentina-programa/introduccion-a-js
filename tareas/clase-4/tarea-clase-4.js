0be60c95685d9f572874f43a2a21b70ba0ee363c
Nicolas Lopez <nicomlopez3@gmail.com>
2022-04-09 14:05:08 -0300

    subiendo la tarea de la clase 4
    subiendo la tarea de la clase 4 con los 3 ejercicios pedidos ya realizados

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -1,8 +1,19 @@
 // TAREA: Imprimí cada 3er número del 3 al 22 usando un 'bucle for'.
 
+for (let i = 3; i <= 22; i += 3) {
+    console.log(i);
+  }
+
 
 // TAREA: Usando un bucle 'while', decile a tu computadora que registre los números de
 //       diez a uno.
+
+
+let numero = 10;
+while (numero > 0) {
+  console.log(numero);
+  numero = numero - 1;
+}
 
 
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
@@ -17,5 +28,39 @@
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
 
+for (let i = 1; i <= 50; i++) {
+    if ((i % 3 === 0) & (i % 5 === 0)) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  
+  for (let i = 1; i <= 50; i = i + 1) {
+    if ((i % 3 === 0) & (i % 5 === 0)) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else {
+      console.log(i);
+    }
+  }
+
 // Calcular el promedio de todos los números en un array de números. (y ponerlo en una función)
 // ej.: calcularPromedio([10,5,4,2,8])
+
+const examenesParciales = [10, 8, 7, 9];
+
+function calcularPromedio(examenesParciales) {
+  let totalNotas = 0;
+  for (i = 0; i < examenesParciales.length; i++) {
+    totalNotas = totalNotas + examenesParciales[i];
+  }
+
+  return totalNotas / examenesParciales.length;
+}
+console.log("El promedio es " + calcularPromedio(examenesParciales));
