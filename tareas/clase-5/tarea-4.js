f40c03e49828ae4b56e5e49a780d8eadcba4f7e2
bastdast <tutebertuccio@gmail.com>
2022-05-12 11:16:59 -0300

    Crea funcionalidades
    Son creadas para poder mostrar al usuario distintos tipos de resultados.

--- tareas/clase-5/tarea-4.js
+++ tareas/clase-5/tarea-4.js
@@ -1,16 +1,54 @@
+/*
 const numerosObtenidos = document.querySelectorAll('li');
 const listaNumeros = [];
 
 for(let i = 0; i < numerosObtenidos.length; i++){
-    listaNumeros.push((numerosObtenidos[i]));
+    listaNumeros.push((numerosObtenidos[i]).value);
 }
 
 console.log(listaNumeros);
+*/
 
-function obtenerPromedio(listaNumeros){}
+const listaNumeros = [1, 2, 2, 3, 4, 5];
 
-function obtenerNumeroPequenio(listaNumeros){}
+function obtenerPromedio(listaNumeros){
+    let sumaNumeros = 0;
+    for(let i = 0; i < listaNumeros.length; i++){
+        sumaNumeros = sumaNumeros + listaNumeros[i];
+    }
+    return sumaNumeros / listaNumeros.length;
+}
 
-function obtenerNumeroGrande(listaNumeros){}
+function obtenerNumeroPequenio(listaNumeros){
+    let numeroMenor = listaNumeros[0];
+    for(let i = 0; i < listaNumeros.length; i++){
+        let numeroCualquiera = listaNumeros[i];
+        if(numeroCualquiera < numeroMenor){
+            numeroMenor = numeroCualquiera;
+        }
+    }
+    return numeroMenor;
+}
 
-function obtenerNumeroFrecuente(listaNumeros){}
+function obtenerNumeroGrande(listaNumeros){
+    let numeroGrande = listaNumeros[0];
+    for(let i = 0; i < listaNumeros.length; i++){
+        let numeroCualquiera = listaNumeros[i];
+        if(numeroCualquiera > numeroGrande){
+            numeroGrande = numeroCualquiera;
+        }
+    }
+    return numeroGrande;
+}
+
+function obtenerNumeroFrecuente(listaNumeros){
+    let numeroFrecuente = 0;
+    for(let i = 0; i < listaNumeros.length; i++){
+        for( let j = i + 1; j < listaNumeros.length; j++){
+            if(listaNumeros[i] === listaNumeros[j]){
+                numeroFrecuente = listaNumeros[i];
+            }
+        }
+    }
+    return numeroFrecuente;
+}
