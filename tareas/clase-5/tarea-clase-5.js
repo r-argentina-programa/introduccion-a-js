db18189c22dbd7727713bc99640e36e7442138af
Thiago <thiagogervassio@gmail.com>
2022-05-14 15:46:58 -0300

    Calculo de Salario
    El usuario ingresa su salario mensual y se le calcula el salario anual

--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -22,6 +22,20 @@ $botonCalcular.onclick = function()
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
+
+document.querySelector("#calcular-salario-mensual").onclick = function(){
+  const $salarioAnual = Number(document.querySelector("#salario-anual").value)
+  const $salarioMensual = calcularSalarioMensual($salarioAnual)
+
+  document.querySelector("#salario-mensual").value = $salarioMensual
+  return false
+}
+
+function calcularSalarioMensual(salarioAnual){
+  return salarioAnual / 12;
+}
+
+
 //TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
 // creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
 // también vamos a crear un <h1> que diga Bienvenido!
