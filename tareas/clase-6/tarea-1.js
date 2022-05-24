2b339a910c872700ecdd3e9b15b77d2a1674256c
bastdast <tutebertuccio@gmail.com>
2022-05-24 11:07:37 -0300

    Añade nueva funcionalidad
    Es creada para que el usuario pueda ingresar la edad de sus parientes.

--- tareas/clase-6/tarea-1.js
+++ tareas/clase-6/tarea-1.js
@@ -3,7 +3,34 @@ const $botonSiguiente = document.querySelector('#siguiente');
 $botonSiguiente.onclick = function(){
     const cantIntegrantes = document.querySelector('#cantidad-integrantes').value;
     const cantidadIntegrantes = Number(cantIntegrantes);
-    console.log(cantidadIntegrantes);
+
+    crearIntegrantes(cantidadIntegrantes);
 
     return false;
+}
+
+const $botonCalcular = document.querySelector('#calcular');
+
+$botonCalcular.onclick = function(){
+
+    return false;
+}
+
+function crearIntegrantes(cantidadIntegrantes){
+    for(let i = 0; i < cantidadIntegrantes; i++){
+        const $div = document.createElement('div');
+        $div.className = 'integrante';
+
+        const $label = document.createElement('label');
+        $label.textContent = (`Integrante #${i + 1}`);
+
+        const $input = document.createElement('input');
+        $input.type = 'number';
+
+        $div.appendChild($label);
+        $div.appendChild($input);
+
+        const $integrantes = document.querySelector('#integrantes');
+        $integrantes.appendChild($div);
+    }   
 }
