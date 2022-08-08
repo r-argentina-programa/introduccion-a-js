74a5534148baec8ecd5b693faff2a67beefc1621
luciima <luciiamar19@gmail.com>
2022-08-08 17:45:56 -0300

    Agrega consigna, crea el HTML con JS
    Modifica el código para crear el form para un número de clases dado por el usuario; agrega la consigna del ejercicio a tarea-4-clase-5.js

--- tareas/clase-5/tarea-4-clase-5.js
+++ tareas/clase-5/tarea-4-clase-5.js
@@ -1,3 +1,35 @@
+//TAREA 4:
+
+//En otro archivo distinto,
+// Por cada clase de r/argentina programa existente, vamos a pedir:
+// horas, minutos y segundos de cada video. Ej. Si un video dura
+// 2 horas, 38 minutos y 20 segundos, vamos a rellenar 3 campos de texto con
+// cada dato.
+// al apretar el botón "Calcular tiempo total", debe mostrar en un
+// <strong> pre-creado el tiempo total de los videos.
+
+function crearFormClaseN(n) {
+    let finalHTMLInForm = "";
+    for(let i=1;i<=n;i++){
+        finalHTMLInForm += `<label>Clase ${i}:</label>
+        <input type="number" class="hours" /> 
+        <input type="number" class="minutes" />
+        <input type="number" class="seconds" /> <br/>`;  
+    }
+    document.querySelector("form").innerHTML += finalHTMLInForm;
+}
+function modificarParrafo(n) {
+    document.querySelector("p").innerText = `Inserte las duraciones de las primeras ${n} clases:`;
+}
+function crearBotonCalcular() {
+    let HTMLInForm = '<input id="boton-calcular-tiempo-videos" type="submit" value="Calcular tiempo total"/>';
+    document.querySelector("form").innerHTML += HTMLInForm;
+}
+let numeroClases = prompt("¿Cuántas clases hubo hasta el momento?",5);
+crearFormClaseN(numeroClases);
+modificarParrafo(numeroClases);
+crearBotonCalcular();
+
 const $botonCalcularTiempoVideos = document.querySelector("#boton-calcular-tiempo-videos");
 const SEGUNDOS_EN_UN_MINUTO = 60;
 const MINUTOS_EN_UNA_HORA = 60; 
