e3ced28efee7c18d3083af8e3d86477fa17348b3
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-08-12 21:55:14 -0300

    Otorga funciones al botón "calcular"

--- tareas/clase-6/tarea-familia.js
+++ tareas/clase-6/tarea-familia.js
@@ -1,1 +1,10 @@
+document.querySelector('#añadir-cantidad-integrantes').onclick = function(event) {
+    const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
+    const cantidadIntegrantes = Number($cantidadIntegrantes.value);
+
+    borrarIntegrantesAnteriores();
+    crearIntegrantes(cantidadIntegrantes);
+
+    event.preventDefault();
+};
 
