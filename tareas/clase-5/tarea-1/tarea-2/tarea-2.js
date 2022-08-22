106ac62f6e694a9efce494c5e49e1ff2abdbf4f0
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-08-22 15:02:32 -0300

    Crea tarea 2 clase 5

---
+++ tareas/clase-5/tarea-1/tarea-2/tarea-2.js
@@ -0,0 +1,22 @@
+const $mostrarInformacion = (document.querySelector('#mostrar-informacion-usuario').onclick = function () {
+    const primerNombreUsuario = document.querySelector('#primer-nombre-usuario').value;
+    const segundoNombreUsuario = document.querySelector('#segundo-nombre-usuario').value;
+    const apellidosUsuario = document.querySelector('#apellidos-usuario').value;
+    const edadUsuario = Number(document.querySelector('#edad-usuario').value);
+
+    const informacionUsuario = capturarInformacion(
+        primerNombreUsuario,
+        segundoNombreUsuario,
+        apellidosUsuario,
+        edadUsuario,
+    );
+    const mensajeAUsuario = 'Bienvenid@';
+    document.querySelector('#bienvenido').innerText = (`${mensajeAUsuario} ${informacionUsuario}`);
+    return false;
+});
+
+function capturarInformacion(primerNombreUsuario, segundoNombreUsuario, apellidosUsuario, edadUsuario) {
+    const informacionUsuario = (`${primerNombreUsuario}  ${segundoNombreUsuario} ${apellidosUsuario} de ${edadUsuario} años`);
+
+    return informacionUsuario;
+}
