30ec0bdc6e119f20d16f2fca4ea13382c5948199
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-01 10:46:07 -0300

    actualiza tarea 4 clase 5

--- tareas/clase-5/tarea-4/tarea-4-clase-5.js
+++ tareas/clase-5/tarea-4/tarea-4-clase-5.js
@@ -1,6 +1,12 @@
 document.querySelector('#calcular-promedio').onclick = function () {
     const listaDeNumeros = document.querySelectorAll('.lista-desordenada');
-    for (let i = 0; i < listaDeNumeros.length; i++) {
-        const element = Number(listaDeNumeros[i].innerHTML);
+   for (let i = 0; i < listaDeNumeros.length; i++) {
+    const numerosDeLaLista = Number(listaDeNumeros[i].innerText);
+    console.log(numerosDeLaLista);
+    
+    let numerosDeLaListaEnArray = 0;
+    numerosDeLaListaEnArray.push(numerosDeLaLista)
+    console.log(numerosDeLaListaEnArray);
+   }
     }
-};
+
