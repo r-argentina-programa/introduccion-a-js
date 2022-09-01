ab8b07d0d12eff81bea937b5e29653a321b9b373
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-01 09:47:12 -0300

    Crea tarea 4 clase 5

--- tareas/clase-5/tarea-4/tarea-4-clase-5.js
+++ tareas/clase-5/tarea-4/tarea-4-clase-5.js
@@ -1,8 +1,6 @@
-document.querySelector("#calcular-promedio").onclick = function(){
-    console.log(Number(document.querySelector(".lista-desordenada").value));
-    const listaNumeros = Number(document.querySelector(".lista-desordenada").value);
-    for (let i = 0; i < listaNumeros.length; i++) {
-        const element = array[i];
-        
+document.querySelector('#calcular-promedio').onclick = function () {
+    const listaDeNumeros = document.querySelectorAll('.lista-desordenada');
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+        const element = Number(listaDeNumeros[i].innerHTML);
     }
-}
+};
