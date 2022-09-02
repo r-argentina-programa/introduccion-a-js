111994320364d6d7f84425edcb9404ee6d45a24b
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-02 01:11:35 -0300

    Actualiza tarea 4 clase 5

--- tareas/clase-5/tarea-4/tarea-4-clase-5.js
+++ tareas/clase-5/tarea-4/tarea-4-clase-5.js
@@ -1,12 +1,17 @@
 document.querySelector('#calcular-promedio').onclick = function () {
-    const listaDeNumeros = document.querySelectorAll('.lista-desordenada');
-   for (let i = 0; i < listaDeNumeros.length; i++) {
-    const numerosDeLaLista = Number(listaDeNumeros[i].innerText);
-    console.log(numerosDeLaLista);
-    
-    let numerosDeLaListaEnArray = 0;
-    numerosDeLaListaEnArray.push(numerosDeLaLista)
-    console.log(numerosDeLaListaEnArray);
-   }
-    }
+    const listaDeNumeros = document.querySelectorAll('.numero');
+    let acumulador = 0;
+    for (let i = 0; i < listaDeNumeros.length; i++) {
+        const numero = Number(listaDeNumeros[i].innerText);
+        console.log(numero);
+        acumulador += numero;
 
+        document.querySelector(
+            '.suma-numeros-total',
+        ).innerText = `La suma total de todos los numeros es: ${acumulador}`;
+
+        const promedio = acumulador / listaDeNumeros.length;
+
+        document.querySelector('.promedio').innerText = `El promedio es: ${promedio}`;
+    }
+};
