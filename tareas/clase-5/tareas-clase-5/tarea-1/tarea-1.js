e25257f4d10029dc4db5d625bfb585ab9e609bab
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-06 18:27:32 -0300

    Crea tarea 1 clase 5

---
+++ tareas/clase-5/tareas-clase-5/tarea-1/tarea-1.js
@@ -0,0 +1,15 @@
+document.querySelector("#calcular-salario-mensual-usuario").onclick = function () {
+    const salarioAnualUsuario = Number(document.querySelector("#salario-anual-usuario").value);
+
+const salarioMensualUsuario = calcularSalarioMensual(salarioAnualUsuario)
+
+    document.querySelector("#salario-mensual-usuario").value = salarioMensualUsuario
+
+    return false;
+}
+
+function calcularSalarioMensual(salarioAnualUsuario){
+    const MESES_EN_EL_ANIO = 12;
+let salarioMensual = salarioAnualUsuario / MESES_EN_EL_ANIO
+return salarioMensual
+}
