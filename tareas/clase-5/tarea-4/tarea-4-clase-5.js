466935b2caf18433b811cecabf0816ab2857be01
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-06 10:10:37 -0300

    actualiza tarea 4 clase 5

--- tareas/clase-5/tarea-4/tarea-4-clase-5.js
+++ tareas/clase-5/tarea-4/tarea-4-clase-5.js
@@ -1,11 +1,18 @@
 document.querySelector('#calcular-promedio').onclick = function () {
     const listaDeNumeros = document.querySelectorAll('.numero');
     let acumulador = 0;
+    let minimo = 999999999999;
     for (let i = 0; i < listaDeNumeros.length; i++) {
         const numero = Number(listaDeNumeros[i].innerText);
         acumulador += numero;
-        for (let i = 0; i < acumulador.length; i++) {}
+
+        if (numero < minimo) {
+            minimo = numero;
+        }
     }
+    console.log('El numero minimo es ' + minimo);
+
+document.querySelector('.numero-pequenio').innerText = `El numero mas pequeño es ${minimo} `
     document.querySelector('.suma-numeros-total').innerText = `La suma total de todos los numeros es: ${acumulador}`;
 
     const promedio = acumulador / listaDeNumeros.length;
