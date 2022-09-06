037369e835c024dde9fbb0def1bc2ec73617abad
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-06 10:37:49 -0300

    corrige tarea 1 clase 5

--- tareas/clase-5/tarea-1/tarea-1.js
+++ tareas/clase-5/tarea-1/tarea-1.js
@@ -1,6 +1,6 @@
 document.querySelector('#calcular-salario-mensual-usuario').onclick = function () {
-    const $salarioAnual = Number(document.querySelector('#salario-anual-usuario').value);
-    const salarioMensual = calcularSalarioMensual($salarioAnual);
+    const salarioAnual = Number(document.querySelector('#salario-anual-usuario').value);
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
 
     document.querySelector('#salario-mensual-usuario').value = salarioMensual;
 
--- tareas/clase-5/tarea-4/tarea-4-clase-5.js
+++ tareas/clase-5/tarea-4/tarea-4-clase-5.js
@@ -12,7 +12,7 @@ document.querySelector('#calcular-promedio').onclick = function () {
     }
     console.log('El numero minimo es ' + minimo);
 
-document.querySelector('.numero-pequenio').innerText = `El numero mas pequeño es ${minimo} `
+document.querySelector('.numero-pequenio').innerText = `El numero mas pequeño es minimo `
     document.querySelector('.suma-numeros-total').innerText = `La suma total de todos los numeros es: ${acumulador}`;
 
     const promedio = acumulador / listaDeNumeros.length;
