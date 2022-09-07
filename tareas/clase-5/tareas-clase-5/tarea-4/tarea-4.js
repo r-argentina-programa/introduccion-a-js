a90e99b895bd5f1af12fdb4097dacbcdc0d0e7f2
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-07 09:45:27 -0300

    Actualiza tarea 4 clase 5

--- tareas/clase-5/tareas-clase-5/tarea-4/tarea-4.js
+++ tareas/clase-5/tareas-clase-5/tarea-4/tarea-4.js
@@ -4,13 +4,30 @@ document.querySelector('#calcular-promedio').onclick = function () {
     for (let i = 0; i < listaDeNumeros.length; i++) {
         const numero = Number(listaDeNumeros[i].innerText);
         acumulador += numero;
-    }
-    document.querySelector('.numero-pequenio').innerText = `El numero mas pequeño es `;
-    document.querySelector('.numero-mayor').innerText = `El numero mas grande es `;
-    document.querySelector('.numero-repetido').innerText = `El numero mas frecuente es `;
+
+    const numeroPequenio = encontrarNumeroMinimo(numero);
+    document.querySelector('.numero-pequenio').innerText = `El numero pequenio es ${numeroPequenio}`;
     document.querySelector('.suma-numeros-total').innerText = `La suma total de todos los numeros es: ${acumulador}`;
 
-    const promedio = acumulador / listaDeNumeros.length;
-
-    document.querySelector('.numero-promedio').innerText = `El promedio es: ${promedio}`;
+    const promedio = calcualPromedio(acumulador, listaDeNumeros);
+    document.querySelector('.numero-promedio').innerText = `El promedio es ${promedio}`;
 };
+
+function calcualPromedio(acumulador, listaDeNumeros) {
+    return acumulador / listaDeNumeros.length;
+}
+
+        function encontrarNumeroMinimo(numero) {
+            let numeroMinimo = numero[0];
+            for (let i = 0; i < numero.length; i++) {
+                let numero = numero[i];
+
+                if (numero < numeroMinimo) {
+                    numeroMinimo = numero;
+                }
+
+                return numeroMinimo;
+            }
+            console.log('EL numero minimo' + numeroMinimo);
+        }
+    }
