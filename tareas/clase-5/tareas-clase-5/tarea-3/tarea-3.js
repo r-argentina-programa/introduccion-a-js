5f529620c998a1459be4a4173045db90b9f6aaaa
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-07 09:50:55 -0300

    Corrige tarea 3

--- tareas/clase-5/tareas-clase-5/tarea-3/tarea-3.js
+++ tareas/clase-5/tareas-clase-5/tarea-3/tarea-3.js
@@ -1,27 +1,27 @@
 const calcularTiempoTotalClases = document.querySelector('#calcular-tiempo-total').onclick = function () {
-    const segundosVideo = document.querySelectorAll('.segundos-video');
+    const segundosDeCadaVideo = document.querySelectorAll('.segundos-video');
     let segundosVideosTotales = 0;
     for (let i = 0; i < segundosVideo.length; i++) {
-        segundosVideosTotales += Number(segundosVideo[i].value);
+        segundosVideosTotales += Number(segundosDeCadaVideo [i].value);
     }
 
     const minutosExtra = Math.floor(segundosVideosTotales / 60);
     const segundosRestantes = segundosVideosTotales % 60;
 
-    const minutosVideo = document.querySelectorAll('.minutos-video');
+    const minutosDeCadaVideo = document.querySelectorAll('.minutos-video');
     let minutosVideosTotales = 0;
     for (let i = 0; i < minutosVideo.length; i++) {
-        minutosVideosTotales += Number(minutosVideo[i].value);
+        minutosVideosTotales += Number(minutosDeCadaVideo[i].value);
     }
     minutosVideosTotales += minutosExtra;
     const horasExtra = Math.floor(minutosVideosTotales / 60);
 
     const minutosRestantes = minutosVideosTotales % 60;
 
-    const horasVideo = document.querySelectorAll('.horas-video');
+    const horasDeCadaVideo = document.querySelectorAll('.horas-video');
     let horasVideoTotales = 0;
     for (let i = 0; i < horasVideo.length; i++) {
-        horasVideoTotales += Number(horasVideo[i].value);
+        horasVideoTotales += Number(horasDeCadaVideo[i].value);
     }
 
     const totalHorasVideo = horasVideoTotales + horasExtra;
