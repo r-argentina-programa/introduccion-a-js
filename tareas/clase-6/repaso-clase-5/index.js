b5f98ed29a71c748546d08b994845a8ee11a1f1f
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-09-09 15:10:28 -0300

    Actualiza clase 6

--- tareas/clase-6/repaso-clase-5/index.js
+++ tareas/clase-6/repaso-clase-5/index.js
@@ -1,17 +1,15 @@
-
-
-function calcularSalarioMensual(salarioAnual){
+function calcularSalarioMensual(salarioAnual) {
     return salarioAnual / 12;
 }
 
-const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual')
+const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual');
 
-$calcularSalarioMensual.onclick = function(){
+$calcularSalarioMensual.onclick = function () {
     const salarioAnual = Number(document.querySelector('#salario-anual').value);
-    const salarioMensual = calcularSalarioMensual(salarioAnual)
-document.querySelector('#salario-mensual').value = salarioMensual
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
+    document.querySelector('#salario-mensual').value = salarioMensual;
 
-console.log(salarioMensual);
+    console.log(salarioMensual);
 
     return false;
-}
+};
