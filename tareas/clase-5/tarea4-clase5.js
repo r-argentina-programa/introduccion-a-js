0f56f39090e9e7f8f95f8f389ec68aeb8ca9dbdc
ignaciomortega <89436962+ignaciomortega@users.noreply.github.com>
2022-11-20 19:21:23 -0300

    Corrige tarea 4
    Cambio de metodología para obtención de datos y prácticas

--- tareas/clase-5/tarea4-clase5.js
+++ tareas/clase-5/tarea4-clase5.js
@@ -1,112 +1,47 @@
-const numerosLista = document.querySelectorAll("li");
-let numeros = [];
-numeros = numeros.sort(function(a, b){return a - b});
-let sumaTotal = 0;
-let promedio = document.querySelector("#promedio");
-let menorNumero = document.querySelector("#menor-numero");
-let mayorNumero = document.querySelector("#mayor-numero");
-let masFrecuente = document.querySelector("#mas-repetido");
-const arrNumerosOrdenados = [];
+const $NUMEROS_LISTA = document.querySelectorAll("li");
+const $NUMERO_MENOR = document.querySelector("#menor-numero");
+const $NUMERO_MAYOR = document.querySelector("#mayor-numero");
+const $PROMEDIO = document.querySelector("#promedio");
+const $MAS_REPETIDO = document.querySelector("#mas-repetido");
+const arrayNumerosLista = [];
 
-for (let i = 0; i < numerosLista.length ; i++){
-    numeros.push(Number(numerosLista[i].innerText));
+
+for (let i = 0 ; i < $NUMEROS_LISTA.length ; i ++){
+        arrayNumerosLista.push($NUMEROS_LISTA[i].innerText);
+        arrayNumerosLista.sort(function(a, b){return a - b})
 }
 
+$NUMERO_MAYOR.innerText = `El mayor número en la lista es ${arrayNumerosLista.at(-1)}`;
+$NUMERO_MENOR.innerText = `El menor número en la lista es ${arrayNumerosLista[0]}`;
+$PROMEDIO.innerText = `El promedio de los numeros de la lista es ${encontrarPromedio(arrayNumerosLista)}`;
+$MAS_REPETIDO.innerText = `El numero mas repetido es ${encontrarNumeroMasRepetido(arrayNumerosLista)}`;
 
-function calcularPromedio(array){
-    for(let i = 0; i < array.length; i++){
-        sumaTotal += array[i];
+function encontrarPromedio(arrayNumeros){
+    let sumaTotal = 0;
+
+    for (let i = 0 ; i < arrayNumeros.length ; i++){
+        sumaTotal += Number(arrayNumeros[i]);
+    }
+    let resultado = sumaTotal / arrayNumeros.length;
+
+    return resultado;
+}
+
+function encontrarNumeroMasRepetido(arrayNumeros){
+    numeroMasFrecuente = arrayNumeros[0];
+    for (let i = 0 ; i < arrayNumeros ; i++){
+        frecuenciaLocal = 0 ;
+        for (let j = 0 ; j < arrayNumeros ; j++){
+            if (numeros[i === numeros[j]]){
+                frecuenciaLocal++
+            }
+        }
+        if (frecuenciaLocal > frecuencia){
+            numeroMasFrecuente = i;
+            frecuencia = frecuenciaLocal;
+        }
     }
 
-    let resultado = sumaTotal / array.length;
-    promedio.innerText = `El promedio de los numeros de la lista es igual a ${resultado}`;
-
+    return numeroMasFrecuente;
+    
 }
-
-function numeroMayor(array){
-    let numero = 0;
-    for(let i = 0; i < array.length ; i++){
-        if (array[i] > numero){
-            numero = array[i];
-        }
-    }
-    mayorNumero.innerText = `El mayor número de la lista es ${numero}`;
-}
-
-function numeroMenor(array){
-    let numero = 50000;
-    for(let i = 0; i < array.length ; i++){
-        if (array[i] < numero){
-            numero = array[i];
-        }
-    }
-    menorNumero.innerText = `El menor número de la lista es ${numero}`;
-}
-
-function ordenarLista(array){
-    const listaOrdenada = array.sort(function(a, b){return a - b});
-
-        for( let i = 0 ; i < listaOrdenada.length; i++){
-            if (listaOrdenada[i] == listaOrdenada[i+1]){
-                arrNumerosOrdenados.push(listaOrdenada[i]);
-            }
-        }    
-}
-
-function numeroMasRepetido(array){
-        let contadorRepeticion = 2;
-        let numerosSinRepetir = [];
-        let cantidadRepeticiones = [];
-        let mayorFrecuencia = 0;
-
-        for( i = 0 ; i < array.length ; i++){
-            if (array[i] === array[i + 1]){
-                contadorRepeticion++;
-            }
-            else {
-                cantidadRepeticiones.push(contadorRepeticion);
-                numerosSinRepetir.push(array[i]);
-                contadorRepeticion = 2;
-            }
-        }
-        
-        for(let i = 0 ; i < cantidadRepeticiones.length ; i++){
-            if (cantidadRepeticiones[i] < cantidadRepeticiones[i+1]){
-                mayorFrecuencia = cantidadRepeticiones[i+1];
-            }
-        }
-
-        function indexMax(array) {
-            if (array === 0) {
-                return -1;
-            }
-        
-            let max = array[0];
-            let maxIndex = 0;
-        
-            for (let i = 1; i < array.length; i++) {
-                if (array[i] > max) {
-                    maxIndex = i;
-                    max = array[i];
-                }
-            }
-        
-            return maxIndex;
-        }
-    let index = indexMax(cantidadRepeticiones);
-
-
-    console.log(numerosSinRepetir);
-    console.log(cantidadRepeticiones);
-    console.log(index);
-
-    masFrecuente.innerText = `El número más repetido, con un total de ${cantidadRepeticiones[index]} apariciones es ${numerosSinRepetir[index]} `
-
-
-}
-
-calcularPromedio(numeros);
-numeroMayor(numeros);
-numeroMenor(numeros);
-ordenarLista(numeros);
-numeroMasRepetido(arrNumerosOrdenados)
