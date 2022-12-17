b7ee055a5afb140d82845e3fbf9598e6ba66be4b
n0g4y0 <n0g4y0@gmail.com>
2022-12-16 20:52:13 -0400

    solucion a la tarea #5 parte 1

--- tareas/clase-5/tarea1.js
+++ tareas/clase-5/tarea1.js
@@ -1,12 +1,14 @@
 document.querySelector('#calcular-salario-mensual').onclick = function(){
-    const salarioAnual = Number(document.querySelector('#salario-anual').value);
-    const salarioMensual = calcularSalarioMensual(salarioAnual);
 
-    document.querySelector('#salario-mensual').value = salarioMensual;
+    const $salarioAnual = Number(document.querySelector('#salario-anual').value);
+    const $salarioMensual = calcularSalarioMensual($salarioAnual);
+
+    document.querySelector('#salario-mensual').value = $salarioMensual;
 
     return false;
 }
 
 function calcularSalarioMensual(salarioAnual){
-    return salarioAnual / 12;
+    const MESES_ANIOS = 12;
+    return salarioAnual / MESES_ANIOS;
 }
