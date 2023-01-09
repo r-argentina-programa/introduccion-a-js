bc161b983184f4f3200c6a7d104b59bb7df348d4
Thiago <thiagogervassio@gmail.com>
2023-01-09 05:52:07 -0300

    se aplico prettier
    se aplico prettier para una mejor lectura del codigo

--- tareas/clase-6/clase6tarea1.js
+++ tareas/clase-6/clase6tarea1.js
@@ -17,22 +17,24 @@ $botonAgregarMiembros.onclick = function () {
 function agregarMiembro(numeroMiembros) {
   const $formulario = document.querySelector(`#formulario`);
   for (i = 1; i <= numeroMiembros; i = i + 1) {
-    const label = document.createElement(`label`)
-    label.textContent = `Edad`
-    $formulario.appendChild(label)
-    const input = document.createElement(`input`)
-    input.type = `number`
-    input.id = `edad-integrante`
-    $formulario.appendChild(input)
+    const label = document.createElement(`label`);
+    label.textContent = `Edad`;
+    $formulario.appendChild(label);
+    const input = document.createElement(`input`);
+    input.type = `number`;
+    input.id = `edad-integrante`;
+    $formulario.appendChild(input);
   }
 }
 
-$botonCalcularEdad.onclick = function(){
-  const $edadIntegrante = document.querySelectorAll(`#edad-integrante`)
-  let edadIntegrante = []
-  for (i = 0; i < $edadIntegrante.length; i = i + 1){
-    let nodos = $edadIntegrante[i]
-    const edades = Number(nodos.value)
-    edadIntegrante.push(edades)
+$botonCalcularEdad.onclick = function () {
+  const $edadIntegrante = document.querySelectorAll(`#edad-integrante`);
+  let edadIntegrante = [];
+  for (i = 0; i < $edadIntegrante.length; i = i + 1) {
+    let nodos = $edadIntegrante[i];
+    const edades = Number(nodos.value);
+    edadIntegrante.push(edades);
   }
-}
+};
+
+function edadPromedio (edadIntegrante)
