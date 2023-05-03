d263799b6f3a7861826a25440ce4a5fb7182e512
Matias Bertuccio <tutebertuccio@hotmail.com>
2023-05-03 10:34:25 -0300

    Crea funcionalidad para extraer las edades
    Se cambia el id de los inputs para poder usar su valor de forma más sencilla para poder guardar el valor de las edades

--- tareas-matutuam/clase-6/tarea-1/tarea-1.js
+++ tareas-matutuam/clase-6/tarea-1/tarea-1.js
@@ -18,6 +18,16 @@ $botonEnviar.onclick = function(event) {
     event.preventDefault();
 }
 
+const $botonCalcular = document.querySelector("#boton-calcular");
+$botonCalcular.onclick = function(event) {
+    const $integrantes = document.querySelectorAll("#integrante");
+    const edadIntegrantes = retornarEdadIntegrantes($integrantes);
+
+    console.log(edadIntegrantes);
+
+    event.preventDefault();
+}
+
 function crearIntegrantes(cantidadIntegrantes) {
 
     for (let i = 0; i < cantidadIntegrantes; i++) {
@@ -29,16 +39,28 @@ function crearIntegrante(i) {
     const $familiares = document.querySelector(".familiares");
 
     const $nuevoIntegrante = document.createElement("div");
-    $nuevoIntegrante.className = "integrante";
+    $nuevoIntegrante.className = `integrante-${i + 1}`;
 
     const $labelNuevo = document.createElement("label");
     $labelNuevo.textContent = `Integrante #${i + 1} - `;
 
     const $nuevoInput = document.createElement("input");
-    $nuevoInput.id = `integrante-${i + 1}`;
+    $nuevoInput.id = `integrante`;
     $nuevoInput.type = "number";
 
     $labelNuevo.appendChild($nuevoInput)
     $nuevoIntegrante.appendChild($labelNuevo);
     $familiares.appendChild($nuevoIntegrante);
+}
+
+function retornarEdadIntegrantes($integrantes) {
+    const edadIntegrantes = [];
+
+    for (let i = 0; i < $integrantes.length; i++) {
+        if (Number($integrantes[i].value) > 0) {
+            edadIntegrantes.push(Number($integrantes[i].value));
+        }
+    }
+
+    return edadIntegrantes;
 }
