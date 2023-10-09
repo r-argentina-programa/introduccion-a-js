d634541f98e871d92d9fd994d691457350e80427
Matias Bertuccio <tutebertuccio@hotmail.com>
2023-10-09 09:46:38 -0300

    Se cambian las pruebas
    Las pruebas son cambiadas para testear el flujo completo de la página

--- cypress/e2e/pruebas.cy.js
+++ cypress/e2e/pruebas.cy.js
@@ -2,35 +2,13 @@
 
 const WEB_URL = "127.0.0.1:8080/tareas-matutuam/clase-5/tarea-1/tarea-1.html";
 
-describe("Testea la calculadora de salarios mensuales", () => {
-  beforeEach(() => {
-    cy.visit(WEB_URL);
+describe('Pruebas para la página de cálculo de salario', () => {
+  it('Debería ingresar un salario anual y obtener un cálculo mensual', () => {
+    cy.visit(WEB_URL); 
+    cy.get('#salario-anual').type('60000');
+    cy.get('#boton-calcular').click();
+    cy.get('#salario-mensual')
+      .should('be.disabled')
+      .should('have.value', '5000');
   });
-
-  it("Chequea que el usuario haya ingresado un salario anual y calcule el salario mensual", () => {
-    const salarioAnual = 500000;
-
-    cy.get("#salario-anual").type(salarioAnual);
-
-    cy.get("#salario-anual").should(($input) => {
-      const valor = parseFloat($input.val());
-      expect(valor).to.be.gte(0);
-    });
-  });
-
-  afterEach(() => {
-    cy.get("button").click();
-  });
-
-  it("Chequea el después de ingresar un salario anual se muestre el salario mensual", () => {
-    const salarioMensual = 500000;
-
-    {force: true}
-    cy.get("#salario-mensual").type(salarioMensual, {force:true});
-
-    cy.get("#salario-mensual").should(($input) => {
-      const valor = parseFloat($input.val());
-      expect(valor).to.be.gte(0);
-    });
-  })
 });
