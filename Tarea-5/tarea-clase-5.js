2a6873c246cb62611cad3d906cf3d41bcd5af054
Marcos <marcos.jinno@gmail.com>
2023-12-29 11:15:35 -0300

    Primer tarea de la clase 5

---
+++ Tarea-5/tarea-clase-5.js
@@ -0,0 +1,47 @@
+/*
+Cosas a tener en cuenta:
+1. Los <input> no tienen .innerText, en vez de eso, usan .value. https://developer.mozilla.org/es/docs/Web/HTML/Elemento/input
+
+2. Los demás elementos usan .innerText para acceder y modificar al texto que aparece dentro. https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+También pueden usar .textContent, las diferencias no son importantes por ahora.
+
+3. Para evitar que el formulario <form> se “mande” y por ende recargue la página,
+al event handler “onclick”, agréguentle un return false; al final de la función.
+
+Ejemplo:
+const $botonCalcular = document.querySelector("#boton-calcular");
+
+$botonCalcular.onclick = function()
+    // código que se ejecuta cuando le hagan click a este elemento...
+    return false;
+}
+*/
+
+//TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
+
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
+
+const $body = document.querySelector('body');
+
+const $botonCalcular = document.querySelector('#boton-calcular'); 
+const $salarioAnual = document.querySelector('#salario-anual');
+const $salarioMensual = document.querySelector('#salario-mensual');
+
+function calcularSalarioMensual(salarioAnual){
+    const MESES_EN_EL_ANIO = 12;
+    const salarioMensual = salarioAnual.value / MESES_EN_EL_ANIO;
+    
+    return salarioMensual;
+}
+
+
+
+
+$botonCalcular.onclick = function(){ 
+    $salarioMensual.value = calcularSalarioMensual($salarioAnual);    
+}
+
+
